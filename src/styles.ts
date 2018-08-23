17a08b9b39a639ca8a9e0ee9f52095081efdf2fb
Ruddy Cahyadi <ruddycahyadi@gmail.com>
2018-08-23 15:35:47 +0700

    moemedh

---
+++ src/styles.ts
@@ -0,0 +1,48 @@
+import { Theme, createStyles } from '@material-ui/core';
+
+const drawerWidth = 240;
+const styles = (theme: Theme) => createStyles({
+    root: {
+        width: '100%',
+        height: '100%',
+        zIndex: 1,
+        overflow: 'hidden',
+    },
+    appFrame: {
+        position: 'relative',
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        position: 'absolute',
+    },
+    navIconHide: {
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    drawerHeader: theme.mixins.toolbar,
+    drawerPaper: {
+        width: 250,
+        backgroundColor: theme.palette.background.default,
+        [theme.breakpoints.up('md')]: {
+            width: drawerWidth,
+            position: 'relative',
+            height: '100%',
+        },
+    },
+    content: {
+        backgroundColor: theme.palette.background.default,
+        width: '100%',
+        height: 'calc(100% - 56px)',
+        marginTop: 56,
+        [theme.breakpoints.up('sm')]: {
+            height: 'calc(100% - 64px)',
+            marginTop: 64,
+        },
+    },
+});
+
+export default styles;
