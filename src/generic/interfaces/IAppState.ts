a45b897c8b46e198c20b514258571da58cdbff3e
cptodix <cptodix@gmail.com>
2018-10-16 10:36:18 +0700

    minor fix after merge

--- src/generic/interfaces/IAppState.ts
+++ src/generic/interfaces/IAppState.ts
@@ -1,8 +1,8 @@
 import { IEmployeeMyState } from '@account/classes';
 import { IAccountState } from '@account/classes/states';
 import { ICommonState } from '@common/classes/states';
-import { IFinanceState } from '@finance/classes/states';
 import { IExpenseState } from '@expense/classes/states';
+import { IFinanceState } from '@finance/classes/states';
 import { IAppBarState, ILayoutState, IListBarState, INotificationState } from '@layout/interfaces';
 import { ILookupState } from '@lookup/classes/states';
 import { IProjectState } from '@project/classes/states';
