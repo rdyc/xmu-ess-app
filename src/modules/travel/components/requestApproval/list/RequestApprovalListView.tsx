9824c94c4b0fa51d53f9ece9ec0b37091e7dadc3
Hasan.Khairullah <hassan.khairullah@gmail.com>
2018-10-24 15:02:18 +0700

    after merge from develop

--- src/modules/travel/components/requestApproval/list/RequestApprovalListView.tsx
+++ src/modules/travel/components/requestApproval/list/RequestApprovalListView.tsx
@@ -99,7 +99,8 @@ export const RequestApprovalListView: React.SFC<RequestApprovalListProps> = prop
         >
           {
             response &&
-            response.metadata && 
+            response.metadata &&
+            response.metadata.paginate && 
             <Grid container spacing={24}>
               <Grid item xs={6} sm={6}>
                 <Typography variant="caption" color="primary">
