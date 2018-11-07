3c9f4620f6f598898b96402d6ed00073f3f630c4
Hasan.Khairullah <hassan.khairullah@gmail.com>
2018-11-07 18:43:25 +0700

    after merge

--- src/modules/travel/components/settlementApproval/detail/TravelSettlementApprovalDetail.ts
+++ src/modules/travel/components/settlementApproval/detail/TravelSettlementApprovalDetail.ts
@@ -124,6 +124,7 @@ const handlerCreators: HandleCreators<TravelSettlementApprovalDetailProps, OwnHa
     });
 
     history.push('/travel/approvals/settlement');
+    
   },
   handleSubmitFail: (props: TravelSettlementApprovalDetailProps) => (errors: FormErrors | undefined, dispatch: Dispatch<any>, submitError: any) => {
     const { intl } = props;
