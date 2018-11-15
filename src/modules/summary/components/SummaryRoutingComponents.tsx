06a4839f966f62876658750eb93aa570416335a1
Drajat Pamuji <drajat2610@gmail.com>
2018-11-15 18:51:30 +0700

    report project profitability insyaallah done

--- src/modules/summary/components/SummaryRoutingComponents.tsx
+++ src/modules/summary/components/SummaryRoutingComponents.tsx
@@ -4,12 +4,14 @@ import { Route, RouteComponentProps, Switch } from 'react-router';
 
 import { BillableList } from '@summary/components/billable/BillableList';
 import { Effectiveness } from '@summary/components/effectiveness/Effectiveness';
+import { Profitability } from '@summary/components/profitability/Profitability';
 
 export const SummaryRoutingComponents: React.SFC<RouteComponentProps> = props => (
   <Switch>
     <Layout>
       <Route path={`${props.match.path}/billable/`} component={BillableList}/>
       <Route path={`${props.match.path}/effectiveness/`} component={Effectiveness}/>
+      <Route path={`${props.match.path}/profitability/`} component={Profitability}/>
     </Layout>
   </Switch>
 );
---
+++ src/modules/summary/components/profitability/ProfitabilityProjectView.tsx
@@ -0,0 +1,159 @@
+import { Button, Card, CardContent, CardHeader, Grid, Table, TableBody, TableCell, TableHead, TableRow, WithStyles } from '@material-ui/core';
+import { isWidthDown, WithWidth } from '@material-ui/core/withWidth';
+import { ISummaryAssignmentTimesheet, ISummaryModuleCost, ISummaryProfitabilityProject } from '@summary/classes/response/profitability';
+import * as classNames from 'classnames';
+import * as React from 'react';
+import { FormattedMessage, InjectedIntlProps } from 'react-intl';
+
+interface OwnProps {
+  projects: ISummaryProfitabilityProject[];
+  handleDialogOpen: (fullScreen: boolean, expenses: ISummaryModuleCost[], projectUid: string) => void;
+}
+
+type AllProps
+  = OwnProps
+  & WithStyles
+  & WithWidth
+  & InjectedIntlProps;
+
+export const ProfitabilityProjectView: React.SFC<AllProps> = props => {
+  const { projects, width, classes, intl, handleDialogOpen } = props;
+  
+  const isMobile = isWidthDown('sm', width);
+  const assignmentFields = ['allocated', 'actual', 'remaining', 'progressAssignment', 'actualRate'];
+  const projectFields = ['maxHours', 'allocatedHours', 'actualHours', 'remainHours', 'progress', 'actualRates', 'actualCosts', 'cogs'];
+  
+  const RenderProgressAssignment = (assignments: ISummaryAssignmentTimesheet[]) => {
+    return (
+      <CardContent>
+        <Table
+          padding= "dense"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell numeric>
+                <FormattedMessage id="summary.profitability.tableHead.consultant" />
+              </TableCell>
+              {
+                assignmentFields.map(assignmentField =>
+                  <TableCell numeric>
+                    <FormattedMessage id={`summary.profitability.tableHead.${assignmentField}`} />
+                  </TableCell>
+                )
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              assignments.map(assignment =>
+                <TableRow>
+                  <TableCell>
+                    {assignment.employee && assignment.employee.fullName}
+                  </TableCell>
+                  <TableCell numeric>
+                    {intl.formatNumber(assignment.allocatedHours)}
+                  </TableCell>
+                  <TableCell numeric>
+                    {assignment.actualHours}
+                  </TableCell>
+                  <TableCell numeric>
+                    {assignment.remainHours}
+                  </TableCell>
+                  <TableCell numeric>
+                    {`${assignment.progress} %`}
+                  </TableCell>
+                  <TableCell numeric>
+                    {intl.formatNumber(assignment.actualRate)}
+                  </TableCell>
+                </TableRow>
+              )
+            }
+          </TableBody>
+        </Table>
+      </CardContent>
+    );
+  };
+  const RenderProfitabilityProject = (
+    <Grid container spacing={16}>
+    {
+      projects.map(project =>
+        <Grid item xs={12}>
+          <Card
+            square
+            className= {!isMobile ? classNames(classes.reportPaperPartial) : classNames(classes.reportPaperPartialMobile)}
+          >
+            <CardHeader
+              title={`${project.projectUid} - ${project.name}`}
+            />
+            <div
+              className= {classNames(classes.reportContentScrollable)}
+            >
+              <CardContent>
+                <Table
+                  className= {classNames(classes.reportTable)}
+                  padding= "dense"
+                >
+                  <TableHead>
+                    <TableRow>
+                      {
+                        projectFields.map(projectField => 
+                          <TableCell 
+                            numeric
+                            className= {classNames(classes.cellWidthXXS)}
+                          >
+                            <FormattedMessage id={`summary.profitability.tableHead.${projectField}`} />
+                          </TableCell>
+                        )
+                      }
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                    <TableCell numeric>
+                        {project.maxHours}
+                      </TableCell>
+                      <TableCell numeric>
+                        {project.allocatedHours}
+                      </TableCell>
+                      <TableCell numeric>
+                        {project.actualHours}
+                      </TableCell>
+                      <TableCell numeric>
+                        {project.remainHours}
+                      </TableCell>
+                      <TableCell numeric>
+                        {`${project.progress} %`}
+                      </TableCell>
+                      <TableCell numeric>
+                        {intl.formatNumber(project.actualRates)}
+                      </TableCell>
+                      <TableCell numeric>
+                        <Button 
+                          variant= "contained"
+                          onClick= {() => handleDialogOpen(isMobile, project.moduleCosts ? project.moduleCosts : [], project.projectUid)}
+                        >
+                          {intl.formatNumber(project.actualCosts)}
+                        </Button>
+                      </TableCell>
+                      <TableCell numeric>
+                        {intl.formatNumber(project.cogs)}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardContent>
+              {
+                project.assignments &&
+                project.assignments.length > 0 &&
+                RenderProgressAssignment(project.assignments)
+                  
+              }
+            </div>
+          </Card>
+        </Grid>
+      )
+    }
+    </Grid>
+  );
+  return RenderProfitabilityProject;
+};
