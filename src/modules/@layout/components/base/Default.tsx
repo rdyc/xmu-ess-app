3afe9e5c9f431bf384e08bdad5f3e317998ff8b5
rdyc <ruddycahyadi@gmail.com>
2019-01-03 13:24:09 +0700

    fixing date feeds

--- src/modules/@layout/components/base/Default.tsx
+++ src/modules/@layout/components/base/Default.tsx
@@ -84,7 +84,7 @@ const styles = (theme: Theme) =>
 
 interface INews {
   source: string;
-  date: Date;
+  date: string;
   url: string;
   image: string;
   title: string;
@@ -94,7 +94,7 @@ interface INews {
 const news: INews[] = [
   {
     source: 'Instagram - equineglobal',
-    date: new Date(2018, 12, 31),
+    date: '2018-12-31',
     url: 'https://www.instagram.com/p/BsCuJSqjrQW/',
     image: 'https://scontent-sin6-2.cdninstagram.com/vp/1517ac38aff300b65b736c95c40a820e/5CC5BD88/t51.2885-15/sh0.08/e35/s750x750/47690400_1187119848111763_6320383304818229248_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com',
     title: 'Happy New Year 2019',
@@ -102,7 +102,7 @@ const news: INews[] = [
   },
   {
     source: 'Instagram - equineglobal',
-    date: new Date(2018, 12, 24),
+    date: '2018-12-24',
     url: 'https://swa.co.id/swa/listed-articles/gandeng-equine-global-kpei-raih-iso-bidang-keamanan-informasi',
     image: 'https://scontent-sin6-1.cdninstagram.com/vp/d918b1dff38c3267469ca9c2d2b19830/5CC74E3B/t51.2885-15/sh0.08/e35/s640x640/47691260_475783666278840_7722909191769161728_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com',
     title: 'Merry Christmas 2018',
@@ -110,7 +110,7 @@ const news: INews[] = [
   },
   {
     source: 'Instagram - equineglobal',
-    date: new Date(2018, 12, 21),
+    date: '2018-12-21',
     url: 'https://www.instagram.com/p/BrpSzepD_P9/',
     image: 'https://scontent-sin6-1.cdninstagram.com/vp/8355ec017b6afa1e7e7e9a9c2fdcb8e9/5CC82A75/t51.2885-15/sh0.08/e35/s640x640/47320332_1973338742748281_5437556900294033408_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com',
     title: 'Friday Inspiring Presentation (FIP)',
@@ -118,7 +118,7 @@ const news: INews[] = [
   },
   {
     source: 'SWA - Business Update',
-    date: new Date(2018, 11, 29),
+    date: '2018-11-29',
     url: 'https://swa.co.id/swa/business-update/equine-technologies-group-menjadi-icon-di-industri-ti-melalui-pengelolaan-sumber-daya-manusia-dan-teknologi',
     image: 'https://s3-ap-southeast-1.amazonaws.com/swa.co.id/wp-content/uploads/2018/11/29140648/equine-4541.gif',
     title: 'EQUINE TECHNOLOGIES GROUP Menjadi Icon di Industri TI melalui Pengelolaan Sumber Daya Manusia dan Teknologi',
@@ -126,7 +126,7 @@ const news: INews[] = [
   },
   {
     source: 'SWA - Listed Articles',
-    date: new Date(2017, 5, 5),
+    date: '2017-05-05',
     url: 'https://swa.co.id/swa/listed-articles/gandeng-equine-global-kpei-raih-iso-bidang-keamanan-informasi',
     image: 'https://s3-ap-southeast-1.amazonaws.com/swa.co.id/wp-content/uploads/2017/05/KPEI-.jpg',
     title: 'Gandeng Equine Global, KPEI Raih ISO Bidang Keamanan Informasi',
@@ -134,7 +134,7 @@ const news: INews[] = [
   },
   {
     source: 'SWA - Profile',
-    date: new Date(2014, 9, 30),
+    date: '2014-09-30',
     url: 'https://swa.co.id/swa/profile/eko-heryanto-tidak-ingin-berhenti-hanya-di-industri-ti',
     image: 'https://s3-ap-southeast-1.amazonaws.com/swa.co.id/wp-content/uploads/2014/09/Eko-Heryanto.jpg.jpg',
     title: 'Eko Heryanto, Tidak Ingin Berhenti Hanya di Industri TI',
