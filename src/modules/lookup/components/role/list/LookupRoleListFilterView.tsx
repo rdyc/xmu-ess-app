c2d12313dc2352f81e772a3e5249d7b732ab4162
Drajat Pamuji <drajat2610@gmail.com>
2019-01-06 08:56:49 +0700

    new role list

---
+++ src/modules/lookup/components/role/list/LookupRoleListFilterView.tsx
@@ -0,0 +1,89 @@
+import { layoutMessage } from '@layout/locales/messages';
+import { LookupCompanyDialog } from '@lookup/components/company/dialog';
+import { lookupMessage } from '@lookup/locales/messages/lookupMessage';
+import {
+  AppBar,
+  Button,
+  Dialog,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import CloseIcon from '@material-ui/icons/Close';
+import ClearIcon from '@material-ui/icons/SettingsBackupRestore';
+import * as React from 'react';
+import { LookupRoleListFilterProps } from './LookupRoleListFilter';
+
+export const LookupRoleListFilterView: React.SFC<LookupRoleListFilterProps> = props => (
+  <React.Fragment>
+    <Dialog
+      fullScreen
+      className={props.layoutState.anchor === 'right' ? props.classes.contentShiftRight : props.classes.contentShiftLeft}
+      disableBackdropClick
+      open={props.isOpen}
+      onClose={props.onClose}
+    >
+      <AppBar className={props.classes.appBarDialog}>
+        <Toolbar>
+          <IconButton color="inherit" onClick={props.onClose} aria-label="Close">
+            <CloseIcon />
+          </IconButton>
+
+          <Typography variant="h6" color="inherit" className={props.classes.flex}>
+            {props.intl.formatMessage(layoutMessage.tooltip.filter)}
+          </Typography>
+
+          {
+            (props.filterCompany) &&
+            <Button color="inherit" onClick={props.handleFilterOnReset}>
+              {props.intl.formatMessage(layoutMessage.action.reset)}
+            </Button>
+          }
+
+          <Button
+            color="inherit"
+            onClick={props.handleFilterOnApply}
+          >
+            {props.intl.formatMessage(layoutMessage.action.apply)}
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <List>
+        <ListItem button onClick={props.handleFilterCompanyVisibility}>
+          <ListItemText
+            primary={props.intl.formatMessage(lookupMessage.role.field.companyUid)}
+            secondary={props.filterCompany && props.filterCompany.name || props.intl.formatMessage(layoutMessage.text.none)}
+          />
+          <ListItemSecondaryAction>
+            {
+              props.filterCompany &&
+              <IconButton onClick={props.handleFilterCompanyOnClear}>
+                <ClearIcon />
+              </IconButton>
+            }
+
+            <IconButton disabled>
+              <ChevronRightIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+        <Divider />
+      </List>
+    </Dialog>
+
+    <LookupCompanyDialog
+      hideBackdrop={true}
+      isOpen={props.isFilterCompanyOpen}
+      onSelected={props.handleFilterCompanyOnSelected}
+      onClose={props.handleFilterCOnClose}
+    />
+
+  </React.Fragment>
+);
