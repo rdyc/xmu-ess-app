4ca60404337c438ee6562c0af25f0f7c7c5ceddc
cptodix <cptodix@gmail.com>
2019-01-14 18:03:33 +0700

    minor fix and adjustments

--- src/modules/expense/helper/expenseFieldTranslator.ts
+++ src/modules/expense/helper/expenseFieldTranslator.ts
@@ -1,9 +1,9 @@
-import { WorkflowStatusType } from '@common/classes/types';
-import { ExpenseField } from '@expense/classes/types';
-import { ICollectionValue } from '@layout/classes/core';
+// import { WorkflowStatusType } from '@common/classes/types';
+// import { ExpenseField } from '@expense/classes/types';
+// import { ICollectionValue } from '@layout/classes/core';
 
-export const expenseFieldTranslator = (find: string, field: ICollectionValue): string => {
-  let result: string = find;
+// export const expenseFieldTranslator = (find: string, field: ICollectionValue): string => {
+  // let result: string = find;
 
   // replace expense type
   // if (field.name === ExpenseField.expenseType) {
@@ -64,55 +64,55 @@ export const expenseFieldTranslator = (find: string, field: ICollectionValue): s
   // }
 
   // replace status type
-  if (field.name === ExpenseField.statusType) {
-    switch (find.toLowerCase()) {
-      case 'submit':
-      case 'submitted':
-        result = WorkflowStatusType.Submitted;
-        break;
+//   if (field.name === ExpenseField.statusType) {
+//     switch (find.toLowerCase()) {
+//       case 'submit':
+//       case 'submitted':
+//         result = WorkflowStatusType.Submitted;
+//         break;
       
-      case 'in progress':
-      case 'on progress':
-      case 'progress':
-        result = WorkflowStatusType.InProgress;
-        break;
+//       case 'in progress':
+//       case 'on progress':
+//       case 'progress':
+//         result = WorkflowStatusType.InProgress;
+//         break;
 
-      case 'approve':
-      case 'approved':
-        result = WorkflowStatusType.Approved;
-        break;
+//       case 'approve':
+//       case 'approved':
+//         result = WorkflowStatusType.Approved;
+//         break;
 
-      case 'reject':
-      case 'rejected':
-        result = WorkflowStatusType.Rejected;
-        break;
+//       case 'reject':
+//       case 'rejected':
+//         result = WorkflowStatusType.Rejected;
+//         break;
 
-      case 'notif':
-      case 'notify':
-        result = WorkflowStatusType.Notify;
-        break;
+//       case 'notif':
+//       case 'notify':
+//         result = WorkflowStatusType.Notify;
+//         break;
 
-      case 'open':
-      case 'opened':
-          result = WorkflowStatusType.Opened;
-          break;
+//       case 'open':
+//       case 'opened':
+//           result = WorkflowStatusType.Opened;
+//           break;
       
-      case 'reopen':
-      case 'reopened':
-      case 're open':
-      case 're opened':
-        result = WorkflowStatusType.ReOpened;
-        break;
+//       case 'reopen':
+//       case 'reopened':
+//       case 're open':
+//       case 're opened':
+//         result = WorkflowStatusType.ReOpened;
+//         break;
 
-      case 'close':
-      case 'closed':
-        result = WorkflowStatusType.Closed;
-        break;
+//       case 'close':
+//       case 'closed':
+//         result = WorkflowStatusType.Closed;
+//         break;
 
-      default:
-        break;
-    }
-  }
+//       default:
+//         break;
+//     }
+//   }
 
-  return result;
-};
+//   return result;
+// };
