003ea277a405368f4deecf54d8e3dea96fe65149
cptodix <cptodix@gmail.com>
2019-01-31 17:03:56 +0700

    ordering edit announcement

--- src/modules/lookup/components/gallery/announcement/AnnouncementEditor.ts
+++ src/modules/lookup/components/gallery/announcement/AnnouncementEditor.ts
@@ -2,6 +2,7 @@ import AppMenu from '@constants/AppMenu';
 import { IAnnouncement } from '@home/classes/response/announcement';
 import { withAnnouncement, WithAnnouncement } from '@home/hoc/withAnnouncement';
 import { WithLayout, withLayout } from '@layout/hoc/withLayout';
+import { IGallery } from '@lookup/classes/response/gallery';
 import { WithWidth } from '@material-ui/core/withWidth';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
 import { RouteComponentProps } from 'react-router';
@@ -18,7 +19,6 @@ import {
   withStateHandlers,
 } from 'recompose';
 import { AnnouncementEditorView } from './AnnouncementEditorView';
-import { IGallery } from '@lookup/classes/response/gallery';
 
 interface OwnOption {
 }
@@ -26,16 +26,19 @@ interface OwnOption {
 interface OwnState {
   imageUid?: string;
   announcementImages: IAnnouncementImage[];
+  loadImages: boolean;
 }
 
 interface OwnHandlers {
   handleSetAnnouncementImages: (images: IAnnouncement[]) => void;
-  handleAddAnnouncementImages: (imageGallery: IGallery) => void;
-  handleRemoveAnnouncementImages: (imageUid: string) => void;
+  handleAddAnnouncementImage: (imageGallery: IGallery) => void;
+  handleRemoveAnnouncementImage: (imageUid: string) => void;
+  handleMoveAnnouncementImage: (index: number, direction: 'forward' | 'backward') => void;
 }
 
 interface OwnStateUpdater extends StateHandlerMap<OwnState> {
   updateImages: StateHandler<OwnState>;
+  afterLoad: StateHandler<OwnState>;
 }
 
 export interface IAnnouncementImage {
@@ -58,6 +61,7 @@ export type AnnouncementEditorProps
 
 const createProps: mapper<AnnouncementEditorProps, OwnState> = (props: AnnouncementEditorProps): OwnState => ({ 
   announcementImages: [],
+  loadImages: true,
 });
 
 const handlerCreators: HandleCreators<AnnouncementEditorProps, OwnHandlers> = {
@@ -66,57 +70,75 @@ const handlerCreators: HandleCreators<AnnouncementEditorProps, OwnHandlers> = {
       ({
       imageUid: item.imageUid,
       imageName: item.name,
-      order: index - 1,
+      order: index + 1,
       imgPath: item.path && item.path.medium,
       })
     );
 
+    props.afterLoad();
     props.updateImages(announcementImages);
   },
-  handleAddAnnouncementImages: (props: AnnouncementEditorProps) => (imageGallery: IGallery) => {
-    const newAnnouncementImages = props.announcementImages.splice({
-      
+  handleAddAnnouncementImage: (props: AnnouncementEditorProps) => (imageGallery: IGallery) => {
+    const newAnnouncementImages = props.announcementImages.splice(props.announcementImages.length, 0, {
+      imageUid: imageGallery.uid,
+      imageName: imageGallery.name,
+      order: props.announcementImages.length + 1,
+      imgPath: imageGallery.path && imageGallery.path.small,
     });
 
-    const announcementImages: IAnnouncementImage[] = newAnnouncementImages.map((item, index) => 
-      ({
-      imageUid: item.imageUid,
-      imageName: item.imageName,
-      order: index - 1,
-      imgPath: item.imgPath,
-      })
-    );
-
-    props.updateImages(announcementImages);
+    rearrangeImages(props, newAnnouncementImages);
   },
-  handleRemoveAnnouncementImages: (props: AnnouncementEditorProps) => (imageUid: string) => {
+  handleRemoveAnnouncementImage: (props: AnnouncementEditorProps) => (imageUid: string) => {
     const newAnnouncementImages = props.announcementImages.filter(image => {
       return image.imageUid !== imageUid;
     });
 
-    const announcementImages: IAnnouncementImage[] = newAnnouncementImages.map((item, index) => 
+    rearrangeImages(props, newAnnouncementImages);
+  },
+  handleMoveAnnouncementImage: (props: AnnouncementEditorProps) => (index: number, direction: 'forward' | 'backward') => {
+    const newAnnouncementImages = props.announcementImages;
+
+    if (direction === 'forward') {
+      const currentImage = newAnnouncementImages[index];
+
+      newAnnouncementImages[index] = newAnnouncementImages[index + 1];
+      newAnnouncementImages[index + 1] = currentImage;
+    } else if (direction === 'backward') {
+      const currentImage = newAnnouncementImages[index];
+
+      newAnnouncementImages[index] = newAnnouncementImages[index - 1];
+      newAnnouncementImages[index - 1] = currentImage;
+    }
+
+    rearrangeImages(props, newAnnouncementImages);
+  },
+};
+
+const rearrangeImages = (props: AnnouncementEditorProps, newImages: IAnnouncementImage[]) => {
+  const announcementImages = newImages.map((item, index) => 
       ({
       imageUid: item.imageUid,
       imageName: item.imageName,
-      order: index - 1,
+      order: index + 1,
       imgPath: item.imgPath,
       })
     );
 
-    props.updateImages(announcementImages);
-  },
+  props.updateImages(announcementImages);
 };
 
 const stateUpdaters: StateUpdaters<OwnOption, OwnState, OwnStateUpdater> = {
   updateImages: (prevState: OwnState) => (images: IAnnouncementImage[]) => ({
     announcementImages: images
   }),
+  afterLoad: (prevState: OwnState) => () => ({
+    loadImages: false,
+  })
 };
 
 const lifeCycleFunctions: ReactLifeCycleFunctions<AnnouncementEditorProps, OwnState> = {
   componentWillMount() {
-    const { 
-      layoutDispatch    } = this.props;
+    const { layoutDispatch } = this.props;
     const { loadRequest } = this.props.announcementDispatch;
     const { response, isLoading } = this.props.announcementState.all;
 
@@ -124,7 +146,7 @@ const lifeCycleFunctions: ReactLifeCycleFunctions<AnnouncementEditorProps, OwnSt
       view: {
         uid: AppMenu.Gallery,
         parentUid: AppMenu.Lookup,
-        title: '', // intl.formatMessage(organizationMessage.workflowSetup.page.listTitle),
+        title: 'edit slider', // intl.formatMessage(organizationMessage.workflowSetup.page.listTitle),
         subTitle : '' // intl.formatMessage(organizationMessage.workflowSetup.page.listSubHeader)
       },
       status: {
@@ -137,8 +159,7 @@ const lifeCycleFunctions: ReactLifeCycleFunctions<AnnouncementEditorProps, OwnSt
     });
 
     if (!response && !isLoading) {
-      loadRequest({
-      });
+      loadRequest({});
     }
   },
   componentWillUnmount() {
--- src/modules/lookup/components/gallery/announcement/AnnouncementEditorView.tsx
+++ src/modules/lookup/components/gallery/announcement/AnnouncementEditorView.tsx
@@ -1,5 +1,7 @@
 import { layoutMessage } from '@layout/locales/messages';
-import { Grid, GridList, GridListTile, GridListTileBar, IconButton, Typography } from '@material-ui/core';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, IconButton, Typography } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import * as React from 'react';
 import { AnnouncementEditorProps } from './AnnouncementEditor';
@@ -7,27 +9,51 @@ import { AnnouncementEditorProps } from './AnnouncementEditor';
 export const AnnouncementEditorView: React.SFC<AnnouncementEditorProps> = props => {
   const { isLoading, response } = props.announcementState.all;
 
-  const RenderImageList = () => {
-    return (
-      <div>
-        <GridList cellHeight={180} cols={3}>
-          {props.announcementImages.map(image => (
-            <GridListTile key={image.imageUid}>
-              <img src={image.imgPath} alt={image.imageName} />
-              <GridListTileBar
-                title={image.imageName}
-                actionIcon={
-                  <IconButton>
-                    <DeleteForeverIcon />
+  const RenderImageList = () => (
+    <Grid container spacing={16}>
+      {
+        props.announcementImages.map((image, index) => (
+          <Grid item xs={6} sm={4} md={3} key={index} style={{height: '400'}}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  style={{objectFit: 'cover'}}
+                  image={image.imgPath}
+                  title={image.imageName}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {`#${image.order} - ${image.imageName}`}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                {
+                  index > 0 &&
+                  <IconButton onClick={() => props.handleMoveAnnouncementImage(index, 'backward')}>
+                    <ArrowBackIosIcon />
                   </IconButton>
                 }
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-    );
-  };
+                <IconButton onClick={() => props.handleRemoveAnnouncementImage(image.imageUid)}>
+                  <DeleteForeverIcon />
+                </IconButton>
+                {
+                  index < (props.announcementImages.length - 1) &&
+                  <IconButton onClick={() => props.handleMoveAnnouncementImage(index, 'forward')}>
+                    <ArrowForwardIosIcon />
+                  </IconButton>
+                }
+              </CardActions>
+            </Card>
+          </Grid>
+        ))
+      }
+      <Grid item xs={6} sm={4} md={3}>
+      
+      </Grid>
+    </Grid>
+  );
 
   const render = (
     <React.Fragment>
@@ -43,6 +69,7 @@ export const AnnouncementEditorView: React.SFC<AnnouncementEditorProps> = props 
             !isLoading &&
             response &&
             response.data &&
+            props.loadImages &&
             props.handleSetAnnouncementImages(response.data)
           }
           {
