19b516aad5a98d2048c6e7809e8279be4e7019c9
Lunghi Haviv <lunghi.haviv@gmail.com>
2019-02-08 15:15:20 +0700

    Filter for Leave Request Fixed

--- src/modules/leave/classes/filters/request/ILeaveRequestGetAllFilter.ts
+++ src/modules/leave/classes/filters/request/ILeaveRequestGetAllFilter.ts
@@ -3,6 +3,5 @@ import { IBasePagingFilter } from '@generic/interfaces';
 export interface ILeaveRequestGetAllFilter extends IBasePagingFilter {
   leaveType?: string;
   statusType?: string;
-  status?: 'pending' | 'complete' | string;
   isRejected?: boolean;
 }
