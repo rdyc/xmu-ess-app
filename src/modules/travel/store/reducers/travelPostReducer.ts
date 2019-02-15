0a026108c1b2059c27b410c302e044d287da5454
Hasan.Khairullah <hassan.khairullah@gmail.com>
2019-02-15 14:53:14 +0700

    fix missing iserror on travel reducersss.

--- src/modules/travel/store/reducers/travelPostReducer.ts
+++ src/modules/travel/store/reducers/travelPostReducer.ts
@@ -14,8 +14,8 @@ const initialState: IQuerySingleState<ITravelPostRequest, ITravelRequest> = {
 
 const reducer: Reducer<IQuerySingleState<ITravelPostRequest, ITravelRequest>> = (state = initialState, action) => {
   switch (action.type) {
-    case Action.POST_REQUEST: return { ...state, isLoading: true, request: action.payload };
-    case Action.POST_SUCCESS: return { ...state, isLoading: false, response: action.payload };
+    case Action.POST_REQUEST: return { ...state, isLoading: true, isError: false, request: action.payload };
+    case Action.POST_SUCCESS: return { ...state, isLoading: false, isError: false, response: action.payload };
     case Action.POST_ERROR: return { ...state, isLoading: false, isError: true, errors: action.payload };
     case Action.POST_DISPOSE: return initialState;
     
