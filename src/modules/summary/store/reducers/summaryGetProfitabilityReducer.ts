ae9d1a875fad3f3c37b0cebce99438c00c413897
Drajat Pamuji <drajat2610@gmail.com>
2019-02-15 14:14:41 +0700

    add isError in reducers

--- src/modules/summary/store/reducers/summaryGetProfitabilityReducer.ts
+++ src/modules/summary/store/reducers/summaryGetProfitabilityReducer.ts
@@ -14,8 +14,8 @@ const initialState: IQuerySingleState<ISummaryGetProfitabilityRequest, ISummaryP
 
 const reducer: Reducer<IQuerySingleState<ISummaryGetProfitabilityRequest, ISummaryProfitability>> = (state = initialState, action) => {
   switch (action.type) {
-    case Action.GET_PROFITABILITY_REQUEST: return { ...state, isLoading: true, request: action.payload };
-    case Action.GET_PROFITABILITY_SUCCESS: return { ...state, isLoading: false, response: action.payload };
+    case Action.GET_PROFITABILITY_REQUEST: return { ...state, isLoading: true, isError: false, request: action.payload };
+    case Action.GET_PROFITABILITY_SUCCESS: return { ...state, isLoading: false, isError: false, response: action.payload };
     case Action.GET_PROFITABILITY_ERROR: return { ...state, isLoading: false, isError: true, errors: action.payload };
     case Action.GET_PROFITABILITY_DISPOSE: return { ...state, ...initialState };
     
