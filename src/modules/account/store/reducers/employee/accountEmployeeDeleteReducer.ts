815b1be55a211950b636bd4fca7e2f065ee8a982
hammadaf9408 <hammadaf9408@gmail.com>
2019-02-15 14:28:51 +0700

    fix missing 'isError' prop in account reducers

--- src/modules/account/store/reducers/employee/accountEmployeeDeleteReducer.ts
+++ src/modules/account/store/reducers/employee/accountEmployeeDeleteReducer.ts
@@ -14,8 +14,8 @@ const initialState: IQuerySingleState<IEmployeeDeleteRequest, boolean> = {
 
 const reducer: Reducer<IQuerySingleState<IEmployeeDeleteRequest, boolean>> = (state = initialState, action) => {
   switch (action.type) {
-    case Action.DELETE_REQUEST: return { ...state, isLoading: true, request: action.payload };
-    case Action.DELETE_SUCCESS: return { ...state, isLoading: false, response: action.payload };
+    case Action.DELETE_REQUEST: return { ...state, isLoading: true, isError: false, request: action.payload };
+    case Action.DELETE_SUCCESS: return { ...state, isLoading: false, isError: false, response: action.payload };
     case Action.DELETE_DISPOSE: return { ...state, isLoading: false, isError: true, errors: action.payload };
     case Action.DELETE_DISPOSE: return { ...state, ...initialState };
 
