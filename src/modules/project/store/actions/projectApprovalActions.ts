7b3347130256c020b270aa24c526b7ba54243c68
rdyc <ruddycahyadi@gmail.com>
2019-02-18 14:09:49 +0700

    enhance project approval actions & sagas

--- src/modules/project/store/actions/projectApprovalActions.ts
+++ src/modules/project/store/actions/projectApprovalActions.ts
@@ -25,17 +25,17 @@ export const enum ProjectApprovalAction {
 // get all
 export const projectApprovalGetAllRequest = (request: IProjectApprovalGetAllRequest) => action(ProjectApprovalAction.GET_ALL_REQUEST, request);
 export const projectApprovalGetAllSuccess = (response: IResponseCollection<IProject>) => action(ProjectApprovalAction.GET_ALL_SUCCESS, response);
-export const projectApprovalGetAllError = (message: string) => action(ProjectApprovalAction.GET_ALL_ERROR, message);
+export const projectApprovalGetAllError = (error: any) => action(ProjectApprovalAction.GET_ALL_ERROR, error);
 export const projectApprovalGetAllDispose = () => action(ProjectApprovalAction.GET_ALL_DISPOSE);
 
 // get by id
 export const projectApprovalGetByIdRequest = (request: IProjectApprovalGetByIdRequest) => action(ProjectApprovalAction.GET_BY_ID_REQUEST, request);
 export const projectApprovalGetByIdSuccess = (response: IResponseSingle<IProjectDetail>) => action(ProjectApprovalAction.GET_BY_ID_SUCCESS, response);
-export const projectApprovalGetByIdError = (message: string) => action(ProjectApprovalAction.GET_BY_ID_ERROR, message);
+export const projectApprovalGetByIdError = (error: any) => action(ProjectApprovalAction.GET_BY_ID_ERROR, error);
 export const projectApprovalGetByIdDispose = () => action(ProjectApprovalAction.GET_BY_ID_DISPOSE);
 
 // post
 export const projectApprovalPostRequest = (request: IProjectApprovalPostRequest) => action(ProjectApprovalAction.POST_REQUEST, request);
 export const projectApprovalPostSuccess = (response: boolean) => action(ProjectApprovalAction.POST_SUCCESS, response);
-export const projectApprovalPostError = (message: string) => action(ProjectApprovalAction.POST_ERROR, message);
+export const projectApprovalPostError = (error: any) => action(ProjectApprovalAction.POST_ERROR, error);
 export const projectApprovalPostDispose = () => action(ProjectApprovalAction.POST_DISPOSE);
