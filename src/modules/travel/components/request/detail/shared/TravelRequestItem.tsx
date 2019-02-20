5b2f42cb52fbf8da35c49530b35ee0e7f4c3c8d0
rdyc <ruddycahyadi@gmail.com>
2019-02-20 18:02:31 +0700

    enhance travel req item view

--- src/modules/travel/components/request/detail/shared/TravelRequestItem.tsx
+++ src/modules/travel/components/request/detail/shared/TravelRequestItem.tsx
@@ -1,9 +1,9 @@
+import { initialName } from '@layout/helper/initialName';
 import { GlobalFormat } from '@layout/types';
 import { GlobalStyle } from '@layout/types/GlobalStyle';
 import {
   Avatar,
   Card,
-  CardContent,
   CardHeader,
   Checkbox,
   Collapse,
@@ -15,215 +15,231 @@ import {
   ListItemSecondaryAction,
   ListItemText,
   TextField,
+  WithStyles,
+  withStyles,
 } from '@material-ui/core';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import PersonIcon from '@material-ui/icons/Person';
+import styles from '@styles';
 import { ITravelRequestItem } from '@travel/classes/response';
 import { travelMessage } from '@travel/locales/messages/travelMessage';
 import * as React from 'react';
-import { FormattedMessage, InjectedIntlProps, injectIntl } from 'react-intl';
-import { compose, mapper, StateHandlerMap, StateUpdaters, withStateHandlers } from 'recompose';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { compose, mapper, setDisplayName, StateHandlerMap, StateUpdaters, withStateHandlers } from 'recompose';
 
-interface OwnProps {
-  data: ITravelRequestItem[] | null | undefined;
+interface IOwnProps {
+  data?: ITravelRequestItem[];
 }
 
-interface OwnState {
-  active: string | undefined;
+interface IOwnState {
+  active?: string;
   isExpanded: boolean;
 }
 
-interface OwnStateHandler extends StateHandlerMap<OwnState> {
-  handleToggle: (type: string) => OwnState;
+interface IOwnStateHandler extends StateHandlerMap<IOwnState> {
+  handleToggle: (type: string) => IOwnState;
 }
 
 type AllProps 
-  = OwnProps
-  & OwnState
-  & OwnStateHandler
-  & InjectedIntlProps;
+  = IOwnProps
+  & IOwnState
+  & IOwnStateHandler
+  & InjectedIntlProps
+  & WithStyles<typeof styles>;
 
-const travelRequestItem: React.SFC<AllProps> = props => {
-  const { data, intl, active, isExpanded, handleToggle } = props;
+const TravelRequestItemView: React.SFC<AllProps> = props => (
+  <Card square>
+    <CardHeader
+      title={props.intl.formatMessage(travelMessage.request.section.itemTitle)}
+      // subheader={props.intl.formatMessage(travelMessage.request.section.itemSubHeader)}
+    />
+    <List>
+      {
+        props.data &&
+        props.data.length === 0 &&
+        <ListItem>
+          <ListItemText
+            primary={props.intl.formatMessage(travelMessage.request.message.emptyItem)}
+            primaryTypographyProps={{align: 'center'}} 
+          />
+        </ListItem>
+      }
 
-  const render = (
-    <Card square>
-      <CardHeader
-        title={props.intl.formatMessage(travelMessage.request.section.itemTitle)}
-        // subheader={props.intl.formatMessage(travelMessage.request.section.itemSubHeader)}
-      />
-      <CardContent>
-        <List>
-          {
-            data &&
-            data.length === 0 &&
-            <ListItem>
+      {
+        props.data &&
+        props.data.map(item => 
+          item.employee &&
+          <React.Fragment key={item.uid}>
+            <Divider />
+
+            <ListItem
+              selected={item.uid === props.active && props.isExpanded}
+              button
+              onClick={() => props.handleToggle(item.uid)}
+            >
+              <ListItemAvatar>
+                <Avatar className={props.classes.avatarSecondary}>
+                  {initialName(item.employee.fullName)}
+                </Avatar>
+              </ListItemAvatar>
               <ListItemText
-                primary={<FormattedMessage id="travel.request.message.items.empty" />}
-                primaryTypographyProps={{align: 'center'}} 
+                primary={item.employee.fullName} 
+                secondary={item.employee.email}
               />
+              <ListItemSecondaryAction>
+                {props.active === item.uid && props.isExpanded ? <ExpandLess /> : <ExpandMore />}
+              </ListItemSecondaryAction>
             </ListItem>
-          }
-          {
-            data &&
-            data.map(item => 
-              item.employee &&
-              <div key={item.uid}>
-                <ListItem
-                  disableGutters
-                  selected={item.uid === active && isExpanded}
-                  button
-                  onClick={() => handleToggle(item.uid)}
-                >
-                  <ListItemAvatar>
-                    <Avatar
-                      alt={item.employee.fullName}
-                    >
-                      <PersonIcon/>
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={item.employee.fullName}
-                  />
-                  <ListItemSecondaryAction>
-                    {active === item.uid && isExpanded ? <ExpandLess /> : <ExpandMore />}
-                  </ListItemSecondaryAction>
-                </ListItem>
-                <Collapse
-                  in={active === item.uid && isExpanded}
-                  timeout="auto"
-                  unmountOnExit
-                >
-                  <FormControlLabel
-                    control={
-                      <Checkbox checked={item.isRoundTrip} />
-                    }
-                    label={props.intl.formatMessage(travelMessage.request.field.isRoundTrip)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly} 
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.transportType)}
-                    value={item.transport ? item.transport.value : '-'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.from)}
-                    value={item.from || '-'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.destination)}
-                    value={item.destination || '-'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.itemStart)}
-                    value={intl.formatDate(item.departureDate, GlobalFormat.TimeDate)}
-                  />                  
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.itemEnd)}
-                    value={intl.formatDate(item.returnDate, GlobalFormat.TimeDate)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.transportCost)}
-                    value={intl.formatNumber(item.costTransport || 0)}
-                  />
-                  <FormControlLabel
-                    control={
-                      <Checkbox checked={item.isTransportByCompany} />
-                    }
-                    label={props.intl.formatMessage(travelMessage.request.field.isTransportByCompany)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.hotel)}
-                    value={item.hotel || '-'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.hotelCost)}
-                    value={intl.formatNumber(item.costHotel || 0)}
-                  />
-                  <FormControlLabel
-                    control={
-                      <Checkbox checked={item.isHotelByCompany} />
-                    }
-                    label={props.intl.formatMessage(travelMessage.request.field.isHotelByCompany)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.note)}
-                    value={item.notes || '-'}
-                  />                    
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.duration)}
-                    value={intl.formatNumber(item.duration || 0)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.currencyUid)}
-                    value={item.currency ? item.currency.name : '-'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.diemValue)}
-                    value={intl.formatNumber(item.diemValue || 0)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.currencyRate)}
-                    value={item.currency ? item.currency.rate : 0}
-                  />
-                                  
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(travelMessage.request.field.amount)}
-                    value={intl.formatNumber(item.amount || 0)}
-                  />
-                  <Divider />
-                </Collapse>
-              </div>)
-          }
-        </List>
 
-        {props.children}
-      </CardContent>
-    </Card>
-  );
-  return render;
-};
+            <Collapse
+              in={props.active === item.uid && props.isExpanded}
+              className={props.classes.paddingFar}
+              timeout="auto"
+              unmountOnExit
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox checked={item.isRoundTrip} />
+                }
+                label={props.intl.formatMessage(travelMessage.request.field.isRoundTrip)}
+              />
 
-const createProps: mapper<AllProps, OwnState> = (props: AllProps): OwnState => ({
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly} 
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.transportType)}
+                value={item.transport ? item.transport.value : '-'}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.from)}
+                value={item.from || '-'}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.destination)}
+                value={item.destination || '-'}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.itemStart)}
+                value={props.intl.formatDate(item.departureDate, GlobalFormat.TimeDate)}
+              />     
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.itemEnd)}
+                value={props.intl.formatDate(item.returnDate, GlobalFormat.TimeDate)}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.transportCost)}
+                value={props.intl.formatNumber(item.costTransport || 0)}
+              />
+
+              <FormControlLabel
+                control={
+                  <Checkbox checked={item.isTransportByCompany} />
+                }
+                label={props.intl.formatMessage(travelMessage.request.field.isTransportByCompany)}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.hotel)}
+                value={item.hotel || '-'}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.hotelCost)}
+                value={props.intl.formatNumber(item.costHotel || 0)}
+              />
+
+              <FormControlLabel
+                control={
+                  <Checkbox checked={item.isHotelByCompany} />
+                }
+                label={props.intl.formatMessage(travelMessage.request.field.isHotelByCompany)}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.note)}
+                value={item.notes || '-'}
+              />    
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.duration)}
+                value={props.intl.formatNumber(item.duration || 0)}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.currencyUid)}
+                value={item.currency ? item.currency.name : '-'}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.diemValue)}
+                value={props.intl.formatNumber(item.diemValue || 0)}
+              />
+
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.currencyRate)}
+                value={item.currency ? item.currency.rate : 0}
+              />
+                              
+              <TextField
+                {...GlobalStyle.TextField.ReadOnly}
+                margin="dense"
+                label={props.intl.formatMessage(travelMessage.request.field.amount)}
+                value={props.intl.formatNumber(item.amount || 0)}
+              />
+            </Collapse>
+          </React.Fragment>
+        )
+      }
+    </List>
+
+    {props.children}
+  </Card>
+);
+
+const createProps: mapper<AllProps, IOwnState> = (props: AllProps): IOwnState => ({
   active: undefined,
   isExpanded: false
 });
 
-const stateUpdaters: StateUpdaters<{}, OwnState, OwnStateHandler> = {
-  handleToggle: (state: OwnState) => (type: string) => ({
+const stateUpdaters: StateUpdaters<{}, IOwnState, IOwnStateHandler> = {
+  handleToggle: (state: IOwnState) => (type: string) => ({
     active: type,
     isExpanded: state.active === type ? !state.isExpanded : true
   })
 };
 
-export const TravelRequestItem = compose<AllProps, OwnProps>(
-  injectIntl,
-  withStateHandlers<OwnState, OwnStateHandler>(createProps, stateUpdaters)
-)(travelRequestItem);
+export const TravelRequestItem = compose<AllProps, IOwnProps>(
+  setDisplayName('TravelRequestItem'),
+  withStateHandlers(createProps, stateUpdaters),
+  withStyles(styles),
+  injectIntl
+)(TravelRequestItemView);
