02e9210c888596d0c6dfe636d2e8fd974cb9d59a
Kristanto Nugraha <33407224+Isoggi@users.noreply.github.com>
2019-02-20 16:19:43 +0700

    Module Request

---
+++ src/modules/purchase/components/purchaseRequest/list/PurchaseRequestList.ts
@@ -0,0 +1,205 @@
+import { IBasePagingFilter } from '@generic/interfaces';
+import { ICollectionValue } from '@layout/classes/core';
+import { IDataBindResult } from '@layout/components/pages';
+import { WithUser, withUser } from '@layout/hoc/withUser';
+import { GlobalFormat } from '@layout/types';
+import { IPurchaseGetAllFilter } from '@purchase/classes/filters/purchaseRequest';
+import { IPurchase } from '@purchase/classes/response/purchaseRequest';
+import { SettlementField } from '@purchase/classes/types';
+import { WithPurchaseRequest, withPurchaseRequest } from '@purchase/hoc/purchaseRequest/withPurchaseRequest';
+import * as moment from 'moment';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { compose, HandleCreators, lifecycle, mapper, ReactLifeCycleFunctions, setDisplayName, shallowEqual, StateHandler, StateHandlerMap, StateUpdaters, withHandlers, withStateHandlers } from 'recompose';
+import { IPurchaseRequestListFilterResult } from './PurchaseRequestListFilter';
+import { PurchaseRequestListView } from './PurchaseRequestListView';
+
+interface IOwnOption {
+  
+}
+
+interface IOwnState extends IPurchaseRequestListFilterResult {
+  fields: ICollectionValue[];
+  isFilterOpen: boolean;
+}
+
+interface IOwnStateUpdater extends StateHandlerMap<IOwnState> {
+  setFilterVisibility: StateHandler<IOwnState>;
+  setFilterApplied: StateHandler<IOwnState>;
+}
+
+interface IOwnHandler {
+  handleOnLoadApi: (filter?: IBasePagingFilter, resetPage?: boolean, isRetry?: boolean) => void;
+  handleOnLoadApiSearch: (find?: string, findBy?: string) => void;
+  handleOnBind: (item: IPurchase, index: number) => IDataBindResult;
+
+  handleFilterVisibility: (event: React.MouseEvent<HTMLElement>) => void;
+  handleFilterApplied: (filter: IPurchaseRequestListFilterResult) => void;
+
+  handleFilterBadge: () => boolean;
+}
+
+export type PurchaseRequestListProps 
+  = IOwnOption
+  & IOwnState
+  & IOwnStateUpdater
+  & IOwnHandler
+  & WithUser
+  & WithPurchaseRequest
+  & InjectedIntlProps
+  & RouteComponentProps;
+
+const createProps: mapper<PurchaseRequestListProps, IOwnState> = (props: PurchaseRequestListProps): IOwnState => {
+  const { request } = props.purchaseRequestState.all;
+  // default state
+  const state: IOwnState = {
+    isFilterOpen: false,
+    // selected: [],
+    fields: Object.keys(SettlementField).map(key => ({
+      value: key,
+      name: SettlementField[key]
+    }))
+  };
+  // When location state are present (ex: redirection from dashboard) then don't use redux state
+  if (props.location.state) {
+    // fill partial props from location state to handle redirection from dashboard notif
+    state.statusType = props.location.state.statusType;
+    state.isSettlement = props.location.state.isSettlement;
+    state.isRejected = props.location.state.isRejected;
+  } else {
+    if (request && request.filter) {
+      state.customerUid = request.filter.customerUid,
+      state.statusType = request.filter.statusType,
+      state.isSettlement = request.filter.isSettlement;
+      state.isRejected = request.filter.isRejected;
+    }
+  }
+
+  return state;
+};
+
+const stateUpdaters: StateUpdaters<PurchaseRequestListProps, IOwnState, IOwnStateUpdater> = {
+  setFilterVisibility: (prevState: IOwnState, props: PurchaseRequestListProps) => () => ({
+    isFilterOpen: !prevState.isFilterOpen
+  }),
+  setFilterApplied: (prevState: IOwnState) => (filter: IPurchaseRequestListFilterResult) => ({
+    ...filter,
+    isFilterOpen: false
+  }),
+};
+
+const handlerCreators: HandleCreators<PurchaseRequestListProps, IOwnHandler> = {
+  handleOnLoadApi: (props: PurchaseRequestListProps) => (params?: IBasePagingFilter, resetPage?: boolean, isRetry?: boolean) => {
+    const { isLoading, request } = props.purchaseRequestState.all;
+    const { loadAllRequest } = props.purchaseRequestDispatch;
+
+    if (props.userState.user && !isLoading) {
+      // predefined filter
+      const filter: IPurchaseGetAllFilter = {
+        companyUid: props.userState.user.company.uid,
+        positionUid: props.userState.user.position.uid,
+        customerUid: props.customerUid,
+        statusType: props.statusType,
+        isRejected: props.isRejected,
+        isSettlement: props.isSettlement,
+        find: request && request.filter && request.filter.find,
+        findBy: request && request.filter && request.filter.findBy,
+        orderBy: params && params.orderBy || request && request.filter && request.filter.orderBy,
+        direction: params && params.direction || request && request.filter && request.filter.direction,
+        page: resetPage ? undefined : params && params.page || request && request.filter && request.filter.page,
+        size: params && params.size || request && request.filter && request.filter.size
+      };
+
+      // when request is defined, then compare the filter props
+      const shouldLoad = !shallowEqual(filter, request && request.filter || {});
+
+      // only load when request parameter are differents
+      if (shouldLoad || isRetry) {
+        loadAllRequest({
+          filter,
+        });
+      }
+    }
+  },
+  handleOnLoadApiSearch: (props: PurchaseRequestListProps) => (find?: string, findBy?: string) => {
+    const { isLoading, request } = props.purchaseRequestState.all;
+    const { loadAllRequest } = props.purchaseRequestDispatch;
+
+    if (props.userState.user && !isLoading) {
+      // predefined filter
+      const filter = {
+        ...request && request.filter,
+        find,
+        findBy,
+        page: undefined,
+        companyUid: props.userState.user.company.uid,
+        positionUid: props.userState.user.position.uid
+      };
+
+      // compare request
+      const shouldLoad = !shallowEqual(filter, request && request.filter || {});
+
+      // only load when request parameter are differents
+      if (shouldLoad) {
+        loadAllRequest({
+          filter,
+        });
+      }
+    }
+  },
+  handleOnBind: (props: PurchaseRequestListProps) => (item: IPurchase, index: number) => ({
+    key: index,
+    primary: item.uid,
+    secondary: item.projectUid || item.project && item.project.name || '',
+    tertiary: item.customer && item.customer.name || item.customerUid || '',
+    quaternary: item.requestIDR && `${props.intl.formatNumber(item.requestIDR, GlobalFormat.CurrencyDefault)}` || '',
+    quinary: item.status && item.status.value || item.statusType || '',
+    senary: item.changes && moment(item.changes.updatedAt ? item.changes.updatedAt : item.changes.createdAt).fromNow() || '?'
+  }),
+  handleFilterVisibility: (props: PurchaseRequestListProps) => (event: React.MouseEvent<HTMLElement>) => {
+    props.setFilterVisibility();
+  },
+  handleFilterApplied: (props: PurchaseRequestListProps) => (filter: IPurchaseRequestListFilterResult) => {
+    props.setFilterApplied(filter);
+  },
+  handleFilterBadge: (props: PurchaseRequestListProps) => () => {
+    return props.customerUid !== undefined ||
+      props.statusType !== undefined ||
+      props.isRejected === true ||
+      props.isSettlement === true;
+  },
+};
+
+const lifecycles: ReactLifeCycleFunctions<PurchaseRequestListProps, IOwnState> = {
+  componentDidUpdate(prevProps: PurchaseRequestListProps) {
+    // track any changes in filter props
+    const isFilterChanged = !shallowEqual(
+      {
+        customerUid: this.props.customerUid,
+        statusType: this.props.statusType,
+        isRejected: this.props.isRejected,
+        isSettlement: this.props.isSettlement
+      },
+      {
+        customerUid: prevProps.customerUid,
+        statusType: prevProps.statusType,
+        isRejected: prevProps.isRejected,
+        isSettlement: prevProps.isSettlement
+      }
+    );
+    if (isFilterChanged) {
+      this.props.handleOnLoadApi(undefined, true);
+    }
+  }
+};
+
+export const PurchaseRequestList = compose(
+  setDisplayName('PurchaseRequestList'),
+  withUser,
+  withPurchaseRequest,
+  withRouter,
+  injectIntl,
+  withStateHandlers(createProps, stateUpdaters),
+  withHandlers(handlerCreators),
+  lifecycle(lifecycles),
+)(PurchaseRequestListView);
---
+++ src/modules/purchase/components/purchaseRequest/list/PurchaseRequestListView.tsx
@@ -0,0 +1,129 @@
+import AppMenu from '@constants/AppMenu';
+import { CollectionPage } from '@layout/components/pages';
+import { SearchBox } from '@layout/components/search';
+import { layoutMessage } from '@layout/locales/messages';
+import { Badge, Button, IconButton, Tooltip } from '@material-ui/core';
+import { AddCircle, CheckCircle, Tune } from '@material-ui/icons';
+import { IPurchase } from '@purchase/classes/response/purchaseRequest';
+import { isRequestEditable, isSettleReady } from '@purchase/helper';
+import { purchaseMessage } from '@purchase/locales/messages/purchaseMessage';
+import * as React from 'react';
+import { PurchaseSummary } from '../detail/shared/PurchaseSummary';
+import { PurchaseRequestListProps } from './PurchaseRequestList';
+import { PurchaseRequestListFilter } from './PurchaseRequestListFilter';
+
+export const PurchaseRequestListView: React.SFC<PurchaseRequestListProps> = props => (
+  <React.Fragment>
+    <CollectionPage
+      // page info
+      info={{
+        uid: AppMenu.PurchaseRequest,
+        parentUid: AppMenu.Purchase,
+        title: props.intl.formatMessage(purchaseMessage.request.pages.listTitle),
+        description: props.intl.formatMessage(purchaseMessage.request.pages.listSubHeader)
+      }}
+
+      // state & fields
+      state={props.purchaseRequestState.all}
+      fields={props.fields}
+
+      // selection
+      // disableSelection={props.handleDisableSelection}
+      // onSelection={props.handleSelection}
+
+      // callback
+      onLoadApi={props.handleOnLoadApi}
+      onBind={props.handleOnBind}
+
+      // row components
+      summaryComponent={(item: IPurchase) => (
+        <PurchaseSummary data={item} />
+      )}
+      actionComponent={(item: IPurchase) => (
+        <React.Fragment>
+          { 
+            item.statusType &&
+            isRequestEditable(item.statusType) &&
+            <Button
+              size="small"
+              onClick={() => props.history.push(`/purchase/requests/form`, { uid: item.uid })}
+            >
+              {props.intl.formatMessage(layoutMessage.action.modify)}
+            </Button>
+          }
+          { 
+            item.statusType &&
+            isSettleReady(item.statusType) &&
+            <Button
+              size="small"
+              onClick={() => props.history.push(`/purchase/settlement/requests/form`, { uid: item.uid })}
+            >
+              {props.intl.formatMessage(layoutMessage.action.modify)}
+            </Button>
+          }
+
+          <Button
+            size="small"
+            onClick={() => props.history.push(`/purchase/requests/${item.uid}`)}
+          >
+            {props.intl.formatMessage(layoutMessage.action.details)}
+          </Button>
+        </React.Fragment>
+      )}
+
+      // app bar component
+      appBarSearchComponent={
+        <SearchBox
+          key="purchase.request"
+          default={props.purchaseRequestState.all.request && props.purchaseRequestState.all.request.filter && props.purchaseRequestState.all.request.filter.find}
+          fields={props.fields}
+          onApply={props.handleOnLoadApiSearch}
+        />
+      }
+      appBarCustomComponent={
+        <IconButton
+          onClick={() => props.history.push('/purchase/requests/form')}
+        >
+          <AddCircle />
+        </IconButton>
+      }
+
+      // data toolbar component
+      toolbarDataComponent={
+        <Tooltip
+          placement="bottom"
+          title={props.intl.formatMessage(layoutMessage.tooltip.filter)}
+        >
+          <div>
+            <IconButton
+              id="option-filter"
+              disabled={props.purchaseRequestState.all.isLoading || props.purchaseRequestState.all.isError}
+              onClick={props.handleFilterVisibility}
+            >
+              <Badge
+                invisible={!props.handleFilterBadge()}
+                badgeContent={
+                  <CheckCircle color="primary" fontSize="small" />
+                }
+              >
+                <Tune />
+              </Badge>
+            </IconButton>
+          </div>
+        </Tooltip>
+      }
+    />
+
+    <PurchaseRequestListFilter
+      isOpen={props.isFilterOpen}
+      initialProps={{
+        customerUid: props.customerUid,
+        statusType: props.statusType,
+        isRejected: props.isRejected,
+        isSettlement: props.isSettlement
+      }}
+      onClose={props.handleFilterVisibility}
+      onApply={props.handleFilterApplied}
+    />
+  </React.Fragment>
+);
