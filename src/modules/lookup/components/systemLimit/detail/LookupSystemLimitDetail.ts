813adc5b01583de4c8f09e8c5af10c440dcdb794
hammadaf9408 <hammadaf9408@gmail.com>
2019-02-21 14:15:12 +0700

    fixing list and detail system limit

--- src/modules/lookup/components/systemLimit/detail/LookupSystemLimitDetail.ts
+++ src/modules/lookup/components/systemLimit/detail/LookupSystemLimitDetail.ts
@@ -1,5 +1,6 @@
 import { WithLayout, withLayout } from '@layout/hoc/withLayout';
 import { WithUser, withUser } from '@layout/hoc/withUser';
+import { IAppBarMenu } from '@layout/interfaces';
 import { layoutMessage } from '@layout/locales/messages';
 import { ISystemLimitDeletePayload } from '@lookup/classes/request';
 import { LookupUserAction } from '@lookup/classes/types';
@@ -10,7 +11,10 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import {
   compose,
   HandleCreators,
+  lifecycle,
   mapper,
+  ReactLifeCycleFunctions,
+  setDisplayName,
   StateHandler,
   StateHandlerMap,
   StateUpdaters,
@@ -20,14 +24,16 @@ import {
 import { Dispatch } from 'redux';
 import { FormErrors } from 'redux-form';
 import { isObject } from 'util';
+
 import { LookupSystemLimitDetailView } from './LookupSystemLimitDetailView';
 
-interface OwnRouteParams {
+interface IOwnRouteParams {
   systemLimitUid: string;
   companyUid: string;
 }
 
-interface OwnHandler {
+interface IOwnHandler {
+  handleOnLoadApi: () => void;
   handleOnOpenDialog: (action: LookupUserAction) => void;
   handleOnCloseDialog: () => void;
   handleOnConfirm: () => void;
@@ -36,7 +42,8 @@ interface OwnHandler {
   handleSubmitFail: (errors: FormErrors | undefined, dispatch: Dispatch<any>, submitError: any) => void;
 }
 
-interface OwnState {
+interface IOwnState {
+  pageOptions?: IAppBarMenu[];
   action?: LookupUserAction;
   dialogFullScreen: boolean;
   dialogOpen: boolean;
@@ -46,35 +53,47 @@ interface OwnState {
   dialogConfirmLabel?: string;
 }
 
-interface OwnStateUpdaters extends StateHandlerMap<OwnState> {
-  stateUpdate: StateHandler<OwnState>;
+interface IOwnStateUpdaters extends StateHandlerMap<IOwnState> {
+  setOptions: StateHandler<IOwnState>;
+  stateUpdate: StateHandler<IOwnState>;
 }
 
 export type SystemLimitDetailProps
   = WithUser
   & WithLayout
   & WithLookupSystemLimit
-  & RouteComponentProps<OwnRouteParams>
+  & RouteComponentProps<IOwnRouteParams>
   & InjectedIntlProps
-  & OwnState
-  & OwnStateUpdaters
-  & OwnHandler;
+  & IOwnState
+  & IOwnStateUpdaters
+  & IOwnHandler;
 
-const createProps: mapper<SystemLimitDetailProps, OwnState> = (props: SystemLimitDetailProps): OwnState => ({
+const createProps: mapper<SystemLimitDetailProps, IOwnState> = (props: SystemLimitDetailProps): IOwnState => ({
   dialogFullScreen: false,
   dialogOpen: false,
   dialogCancelLabel: props.intl.formatMessage(layoutMessage.action.disaggre),
   dialogConfirmLabel: props.intl.formatMessage(layoutMessage.action.aggre)
 });
 
-const stateUpdaters: StateUpdaters<SystemLimitDetailProps, OwnState, OwnStateUpdaters> = {
-  stateUpdate: (prevState: OwnState) => (newState: any) => ({
+const stateUpdaters: StateUpdaters<SystemLimitDetailProps, IOwnState, IOwnStateUpdaters> = {
+  stateUpdate: (prevState: IOwnState) => (newState: any) => ({
     ...prevState,
     ...newState
-  })
+  }),
+  setOptions: (prevState: IOwnState, props: SystemLimitDetailProps) => (options?: IAppBarMenu[]): Partial<IOwnState> => ({
+    pageOptions: options
+  }),
 };
 
-const handlerCreators: HandleCreators<SystemLimitDetailProps, OwnHandler> = {
+const handlerCreators: HandleCreators<SystemLimitDetailProps, IOwnHandler> = {
+  handleOnLoadApi: (props: SystemLimitDetailProps) => () => { 
+    if (props.userState.user && props.match.params.systemLimitUid && !props.systemLimitState.detail.isLoading) {
+      props.systemLimitDispatch.loadDetailRequest({
+        companyUid: props.history.location.state.companyUid,
+        systemLimitUid: props.match.params.systemLimitUid
+      });
+    }
+  },
   handleOnOpenDialog: (props: SystemLimitDetailProps) => (action: LookupUserAction) => {
     if (action === LookupUserAction.Modify) {
       props.stateUpdate({
@@ -168,12 +187,12 @@ const handlerCreators: HandleCreators<SystemLimitDetailProps, OwnHandler> = {
     });
   },
   handleSubmitSuccess: (props: SystemLimitDetailProps) => (response: boolean) => {
-    props.history.push('/lookup/systemlimits/');
-
     props.layoutDispatch.alertAdd({
       time: new Date(),
       message: props.intl.formatMessage(lookupMessage.systemLimit.message.deleteSuccess, { uid : props.match.params.systemLimitUid })
     });
+
+    props.history.push('/lookup/systemlimits/');
   },
   handleSubmitFail: (props: SystemLimitDetailProps) => (errors: FormErrors | undefined, dispatch: Dispatch<any>, submitError: any) => {
     if (errors) {
@@ -191,12 +210,55 @@ const handlerCreators: HandleCreators<SystemLimitDetailProps, OwnHandler> = {
   }
 };
 
+const lifecycles: ReactLifeCycleFunctions<SystemLimitDetailProps, IOwnState> = {
+  componentDidUpdate(prevProps: SystemLimitDetailProps) {
+    // handle updated route params
+    if (this.props.match.params.systemLimitUid !== prevProps.match.params.systemLimitUid) {
+      this.props.handleOnLoadApi();
+    }
+
+    // handle updated response state
+    if (this.props.systemLimitState.detail.response !== prevProps.systemLimitState.detail.response) {
+      const { isLoading } = this.props.systemLimitState.detail;
+
+      // generate option menus
+      const options: IAppBarMenu[] = [
+        {
+          id: LookupUserAction.Refresh,
+          name: this.props.intl.formatMessage(layoutMessage.action.refresh),
+          enabled: !isLoading,
+          visible: true,
+          onClick: this.props.handleOnLoadApi
+        },
+        {
+          id: LookupUserAction.Modify,
+          name: this.props.intl.formatMessage(layoutMessage.action.modify),
+          enabled: true,
+          visible: true,
+          onClick: () => this.props.handleOnOpenDialog(LookupUserAction.Modify)
+        },
+        {
+          id: LookupUserAction.Delete,
+          name: this.props.intl.formatMessage(layoutMessage.action.delete),
+          enabled: true,
+          visible: true,
+          onClick: () => this.props.handleOnOpenDialog(LookupUserAction.Delete)
+        }
+      ];
+
+      this.props.setOptions(options);
+    }
+  }
+};
+
 export const LookupSystemLimitDetail = compose<SystemLimitDetailProps, {}>(
   withRouter,
   withUser,
   withLayout,
   withLookupSystemLimit,
   injectIntl,
-  withStateHandlers<OwnState, OwnStateUpdaters, {}>(createProps, stateUpdaters),
-  withHandlers<SystemLimitDetailProps, OwnHandler>(handlerCreators),
+  withStateHandlers<IOwnState, IOwnStateUpdaters, {}>(createProps, stateUpdaters),
+  withHandlers<SystemLimitDetailProps, IOwnHandler>(handlerCreators),
+  lifecycle(lifecycles),
+  setDisplayName('LookupSystemLimitDetail')
 )(LookupSystemLimitDetailView);
---
+++ src/modules/lookup/components/systemLimit/list/LookupSystemLimitListView.tsx
@@ -0,0 +1,106 @@
+import AppMenu from '@constants/AppMenu';
+import { CollectionPage } from '@layout/components/pages';
+import { SearchBox } from '@layout/components/search';
+import { layoutMessage } from '@layout/locales/messages';
+import { Badge, Button, IconButton, Tooltip } from '@material-ui/core';
+import { AddCircle, CheckCircle, Tune } from '@material-ui/icons';
+import * as React from 'react';
+
+import { ISystemLimit } from '@lookup/classes/response';
+import { lookupMessage } from '@lookup/locales/messages/lookupMessage';
+import { LookupSystemLimitFilter } from './LookupSystemLimitFilter';
+import { LookupSystemLimitListProps } from './LookupSystemLimitList';
+import { LookupSystemLimitSummary } from './LookupSystemLimitSummary';
+
+export const LookupSystemLimitListView: React.SFC<LookupSystemLimitListProps> = props => (
+  <React.Fragment>
+    <CollectionPage 
+      // page info
+      info={{
+        uid: AppMenu.LookupSystemLimit,
+        parentUid: AppMenu.Lookup,
+        title: props.intl.formatMessage(lookupMessage.systemLimit.page.listTitle),
+        description: props.intl.formatMessage(lookupMessage.systemLimit.page.listSubHeader),
+      }}
+
+      // state & fields
+      state={props.systemLimitState.all}
+      fields={props.fields}
+
+      // callback
+      onLoadApi={props.handleOnLoadApi}
+      onBind={props.handleOnBind}
+      
+      // row components
+      summaryComponent={(item: ISystemLimit) => (
+        <LookupSystemLimitSummary data={item} />
+      )}
+      actionComponent={(item: ISystemLimit) => (
+        <React.Fragment>
+          <Button 
+            size="small"
+            onClick={() => props.history.push(`/lookup/systemlimits/form`, { uid: item.uid })}
+          >
+            {props.intl.formatMessage(layoutMessage.action.modify)}            
+          </Button>
+          <Button 
+            size="small"
+            onClick={() => props.history.push(`/lookup/systemlimits/${item.uid}`, { companyUid: item.companyUid })}
+          >
+            {props.intl.formatMessage(layoutMessage.action.details)}
+          </Button>
+        </React.Fragment>
+      )}
+      // app bar component
+      appBarSearchComponent={
+        <SearchBox
+          key="lookup.systemlimit"
+          default={props.systemLimitState.all.request && props.systemLimitState.all.request.filter && props.systemLimitState.all.request.filter.find}
+          fields={props.fields}
+          onApply={props.handleOnLoadApiSearch}
+        />
+      }
+      appBarCustomComponent={
+        <IconButton
+          onClick={() => props.history.push('/lookup/systemlimits/form')}
+        >
+          <AddCircle/>
+        </IconButton>
+      }
+
+      // data toolbar component
+      toolbarDataComponent={
+        <Tooltip
+          placement="bottom"
+          title={props.intl.formatMessage(layoutMessage.tooltip.filter)}
+        >
+          <div>
+            <IconButton
+              id="option-filter"
+              disabled={props.systemLimitState.all.isLoading || props.systemLimitState.all.isError}
+              onClick={props.handleFilterVisibility} 
+            >
+              <Badge
+                invisible={!props.handleFilterBadge()}
+                badgeContent={
+                  <CheckCircle color="primary" fontSize="small" />
+                }
+              >
+                <Tune/>
+              </Badge>
+            </IconButton>
+          </div>
+        </Tooltip>
+      }
+    />
+    <LookupSystemLimitFilter
+      isOpen={props.isFilterOpen}
+      initialProps={{
+        companyUid: props.companyUid,
+        categoryType: props.categoryType
+      }}
+      onClose={props.handleFilterVisibility}
+      onApply={props.handleFilterApplied}
+    />
+  </React.Fragment>
+);
