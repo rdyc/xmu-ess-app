4a290b02e37d3aa747b1b9b4b4ff244cb2389f63
cptodix <cptodix@gmail.com>
2019-03-12 16:31:31 +0700

    add companyuid param

--- src/modules/project/classes/filters/registration/IProjectRegistrationGetListFilter.ts
+++ src/modules/project/classes/filters/registration/IProjectRegistrationGetListFilter.ts
@@ -1,6 +1,7 @@
 import { IBasePagingFilter } from '@generic/interfaces';
 
 export interface IProjectRegistrationGetListFilter extends IBasePagingFilter {
+  companyUid?: string;
   activeOnly?: boolean;
   assignmentStatus?: 'assigned' | 'unassigned';
   customerUids?: string;
