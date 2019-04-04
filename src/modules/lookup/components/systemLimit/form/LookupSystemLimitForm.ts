cfb11dd723750c9f2a6e60478be2c937dfa06928
hammadaf9408 <hammadaf9408@gmail.com>
2019-04-04 14:42:13 +0700

    Formik system limit v1

--- src/modules/lookup/components/systemLimit/form/LookupSystemLimitForm.ts
+++ src/modules/lookup/components/systemLimit/form/LookupSystemLimitForm.ts
@@ -1,8 +1,15 @@
 import { ISystemListFilter } from '@common/classes/filters';
+import { WithCommonSystem, withCommonSystem } from '@common/hoc/withCommonSystem';
+import { FormMode } from '@generic/types';
 import { WithUser, withUser } from '@layout/hoc/withUser';
+import { IValidationErrorResponse } from '@layout/interfaces';
 import { ILookupCompanyGetListFilter } from '@lookup/classes/filters/company';
+import { ISystemLimitPostPayload, ISystemLimitPutPayload } from '@lookup/classes/request';
+import { ISystemLimit } from '@lookup/classes/response';
 import { WithLookupSystemLimit, withLookupSystemLimit } from '@lookup/hoc/withLookupSystemLimit';
 import { lookupMessage } from '@lookup/locales/messages/lookupMessage';
+import { WithStyles, withStyles } from '@material-ui/core';
+import styles from '@styles';
 import { FormikActions } from 'formik';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
 import { RouteComponentProps, withRouter } from 'react-router';
@@ -19,14 +26,15 @@ import {
   withHandlers,
   withStateHandlers,
 } from 'recompose';
+import { isNullOrUndefined } from 'util';
 import * as Yup from 'yup';
 import { SystemLimitFormView } from './LookupSystemLimitFormView';
 
 export interface ISystemLimitFormValue {
   uid?: string;
-  companyUid?: string;
-  categoryType?: string;
-  days?: number;
+  companyUid: string;
+  categoryType: string;
+  days: number;
 }
 
 interface IOwnRouteParams {
@@ -38,37 +46,41 @@ interface IOwnOption {
 }
 
 interface IOwnState {
+  formMode: FormMode;
+
   initialValues?: ISystemLimitFormValue;
   validationSchema?: Yup.ObjectSchema<Yup.Shape<{}, Partial<ISystemLimitFormValue>>>;
 
   filterLookupCompany?: ILookupCompanyGetListFilter;
   filterCommonSystem?: ISystemListFilter;
 
-  dialogFullScreen: boolean;
-  dialogOpen: boolean;
-  dialogTitle?: string;
-  dialogContent?: string;
-  dialogCancelLabel?: string;
-  dialogConfirmLabel?: string;
+  // dialogFullScreen: boolean;
+  // dialogOpen: boolean;
+  // dialogTitle?: string;
+  // dialogContent?: string;
+  // dialogCancelLabel?: string;
+  // dialogConfirmLabel?: string;
 }
 
 interface IOwnStateUpdater extends StateHandlerMap<IOwnState> {
   setInitialValues: StateHandler<IOwnState>;
-  setValidationSchema: StateHandler<IOwnState>;
-  setFilterLookupCompany: StateHandler<IOwnState>;
-  setFilterCommonSystem: StateHandler<IOwnState>;
+  // setValidationSchema: StateHandler<IOwnState>;
+  // setFilterLookupCompany: StateHandler<IOwnState>;
+  // setFilterCommonSystem: StateHandler<IOwnState>;
 }
 
 interface IOwnHandler {
-  handleOnLoadApi: () => void;
-  handleOnCloseDialog: () => void;
-  handleOnConfirm: () => void;
+  handleOnLoadDetail: () => void;
+  // handleOnCloseDialog: () => void;
+  // handleOnConfirm: () => void;
   handleOnSubmit: (values: ISystemLimitFormValue, actions: FormikActions<ISystemLimitFormValue>) => void;
 }
 
 export type SystemLimitFormProps
   = WithLookupSystemLimit
+  & WithCommonSystem
   & WithUser
+  & WithStyles<typeof styles>
   & RouteComponentProps<IOwnRouteParams>
   & InjectedIntlProps
   & IOwnOption
@@ -76,86 +88,219 @@ export type SystemLimitFormProps
   & IOwnStateUpdater
   & IOwnHandler;
 
-const createProps: mapper<IOwnOption, IOwnState> = (props: IOwnOption): IOwnState => ({
-  dialogFullScreen: false,
-  dialogOpen: false
+const createProps: mapper<SystemLimitFormProps, IOwnState> = (props: SystemLimitFormProps): IOwnState => ({
+  // form props
+  formMode: isNullOrUndefined(props.history.location.state) ? FormMode.New : FormMode.Edit,
+  
+  // form values
+  initialValues: {
+    uid: 'Auto Generated',
+    companyUid: '',
+    categoryType: '',
+    days: 0
+  },
+
+  // validation props
+  validationSchema: Yup.object().shape<Partial<ISystemLimitFormValue>>({
+    companyUid: Yup.string()
+      .required(props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('companyUid', 'fieldRequired'))),
+
+    categoryType: Yup.string()
+      .required(props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('categoryType', 'fieldRequired'))),
+
+    days: Yup.number()
+      .min(1)
+      .required(props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('days', 'fieldRequired'))),
+  }),
+
+  // filter props
+  filterLookupCompany: {
+    orderBy: 'name',
+    direction: 'ascending'
+  },
+
+  filterCommonSystem: {
+    orderBy: 'value',
+    direction: 'ascending'
+  }
 });
 
 const stateUpdaters: StateUpdaters<SystemLimitFormProps, IOwnState, IOwnStateUpdater> = {
   setInitialValues: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
     initialValues: values
   }),
-  setValidationSchema: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
-    validationSchema: values
-  }),
-  setFilterLookupCompany: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
-    filterLookupCompany: values
-  }),
-  setFilterCommonSystem: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
-    filterCommonSystem: values
-  })
+  // setValidationSchema: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
+  //   validationSchema: values
+  // }),
+  // setFilterLookupCompany: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
+  //   filterLookupCompany: values
+  // }),
+  // setFilterCommonSystem: (state: IOwnState) => (values: any): Partial<IOwnState> => ({
+  //   filterCommonSystem: values
+  // })
 };
 
 const handlerCreators: HandleCreators<SystemLimitFormProps, IOwnHandler> = {
-  handleOnLoadApi: (props: SystemLimitFormProps) => () => {
-    //
+  handleOnLoadDetail: (props: SystemLimitFormProps) => () => {
+    if (!isNullOrUndefined(props.history.location.state)) {
+      const user = props.userState.user;
+      const systemLimitUid = props.history.location.state.uid;
+      const { isLoading } = props.systemLimitState.detail;
+
+      if (user && systemLimitUid && !isLoading) {
+        props.systemLimitDispatch.loadDetailRequest({
+          systemLimitUid,
+          companyUid: props.history.location.state.companyUid,
+        });
+      }
+    }
   },
-  handleOnCloseDialog: (props: SystemLimitFormProps) => () => {
-    //
-  },
-  handleOnConfirm: (props: SystemLimitFormProps) => () => {
-    //
-  },
+  // handleOnCloseDialog: (props: SystemLimitFormProps) => () => {
+  //   //
+  // },
+  // handleOnConfirm: (props: SystemLimitFormProps) => () => {
+  //   //
+  // },
   handleOnSubmit: (props: SystemLimitFormProps) => (values: ISystemLimitFormValue, actions: FormikActions<ISystemLimitFormValue>) => {
-    //
+    const { user } = props.userState;
+    let promise = new Promise((resolve, reject) => undefined);
+
+    if (user) {
+      // New
+      if (props.formMode === FormMode.New) {
+        // fill payload
+        const payload: ISystemLimitPostPayload = {
+          categoryType: values.categoryType,
+          days: values.days
+        };
+
+        // set the promise
+        promise = new Promise((resolve, reject) => {
+          props.systemLimitDispatch.createRequest({
+            resolve,
+            reject,
+            companyUid: values.companyUid,
+            data: payload
+          });
+        });
+      }
+
+      // Edit
+      if (props.formMode === FormMode.Edit) {
+        const systemLimitUid = props.history.location.state.uid;
+        const companyUid = props.history.location.state.companyUid;
+
+        // must have systemlimitUid
+        if (systemLimitUid && companyUid) {
+          const payload: ISystemLimitPutPayload = {
+            categoryType: values.categoryType,
+            days: values.days
+          };
+
+          // set the promise
+          promise = new Promise((resolve, reject) => {
+            props.systemLimitDispatch.updateRequest({
+              systemLimitUid,
+              resolve,
+              reject,
+              companyUid: values.companyUid,
+              data: payload as ISystemLimitPutPayload
+            });
+          });
+        }
+      }
+    }
+
+    // handling promise
+    promise
+      .then((response: ISystemLimit) => {
+        console.log(response);
+
+        // set submitting status
+        actions.setSubmitting(false);
+
+        // clear form status
+        actions.setStatus();
+
+        props.history.push(`/lookup/systemlimits/${response.uid}`, { companyuid: response.companyUid });
+      })
+      .catch((error: IValidationErrorResponse) => {
+        // set submitting status
+        actions.setSubmitting(false);
+        
+        // set form status
+        actions.setStatus(error);
+        
+        // error on form fields
+        if (error.errors) {
+          error.errors.forEach(item => 
+            actions.setFieldError(item.field, props.intl.formatMessage({id: item.message}))
+          );
+        }
+      });
   }
 };
 
 const lifeCycleFunctions: ReactLifeCycleFunctions<SystemLimitFormProps, IOwnState> = {
   componentDidMount() {
-    // 1. define initial values
-    const initialValues: ISystemLimitFormValue = {
-      companyUid: '',
-      categoryType: '',
-      days: 0
-    };
+    // // 1. define initial values
+    // const initialValues: ISystemLimitFormValue = {
+    //   companyUid: '',
+    //   categoryType: '',
+    //   days: 0
+    // };
 
-    this.props.setInitialValues(initialValues);
+    // this.props.setInitialValues(initialValues);
 
-    // 2. define validation schema
-    const validationSchema = Yup.object().shape<Partial<ISystemLimitFormValue>>({
-      companyUid: Yup.string()
-        .required(this.props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('companyUid', 'fieldRequired'))),
+    // // 2. define validation schema
+    // const validationSchema = Yup.object().shape<Partial<ISystemLimitFormValue>>({
+    //   companyUid: Yup.string()
+    //     .required(this.props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('companyUid', 'fieldRequired'))),
 
-      categoryType: Yup.string()
-        .required(this.props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('categoryType', 'fieldRequired'))),
+    //   categoryType: Yup.string()
+    //     .required(this.props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('categoryType', 'fieldRequired'))),
 
-      days: Yup.number()
-        .min(0)
-        .required(this.props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('days', 'fieldRequired')))
-    });
+    //   days: Yup.number()
+    //     .min(0)
+    //     .required(this.props.intl.formatMessage(lookupMessage.systemLimit.fieldFor('days', 'fieldRequired')))
+    // });
 
-    this.props.setValidationSchema(validationSchema);
+    // this.props.setValidationSchema(validationSchema);
 
-    // 3. define company filter
-    const filterCompany: ILookupCompanyGetListFilter = {
-      orderBy: 'name',
-      direction: 'ascending'
-    };
+    // // 3. define company filter
+    // const filterCompany: ILookupCompanyGetListFilter = {
+    //   orderBy: 'name',
+    //   direction: 'ascending'
+    // };
 
-    this.props.setFilterLookupCompany(filterCompany);
+    // this.props.setFilterLookupCompany(filterCompany);
 
-    // 4. define common project filter
-    const filterCommonSystem: ISystemListFilter = {
-      orderBy: 'value',
-      direction: 'ascending'
-    };
+    // // 4. define common project filter
+    // const filterCommonSystem: ISystemListFilter = {
+    //   orderBy: 'value',
+    //   direction: 'ascending'
+    // };
 
-    this.props.setFilterCommonSystem(filterCommonSystem);
+    // this.props.setFilterCommonSystem(filterCommonSystem);
   },
-  // componentDidUpdate(prevProps: SystemLimitFormProps) {
-  //   console.log('component did update');
-  // }
+  componentDidUpdate(prevProps: SystemLimitFormProps) {
+    const { response: thisResponse } = this.props.systemLimitState.detail;
+    const { response: prevResponse } = prevProps.systemLimitState.detail;
+    
+    if (thisResponse !== prevResponse) {
+      if (thisResponse && thisResponse.data) {
+        // define initial values
+        const initialValues: ISystemLimitFormValue = {
+          uid: thisResponse.data.uid,
+          companyUid: thisResponse.data.companyUid,
+          categoryType: thisResponse.data.categoryType,
+          days: thisResponse.data.days
+        };
+
+        this.props.setInitialValues(initialValues);
+      }
+    }
+  }
 };
 
 export const SystemLimitForm = compose<SystemLimitFormProps, IOwnOption>(
@@ -163,8 +308,10 @@ export const SystemLimitForm = compose<SystemLimitFormProps, IOwnOption>(
   withUser,
   withRouter,
   withLookupSystemLimit,
+  withCommonSystem,
   injectIntl,
   withStateHandlers(createProps, stateUpdaters),
   withHandlers(handlerCreators),
-  lifecycle(lifeCycleFunctions)
+  lifecycle(lifeCycleFunctions),
+  withStyles(styles)
 )(SystemLimitFormView);
--- src/modules/lookup/components/systemLimit/form/LookupSystemLimitFormView.tsx
+++ src/modules/lookup/components/systemLimit/form/LookupSystemLimitFormView.tsx
@@ -1,14 +1,14 @@
-import { CommonSystemOption } from '@common/components/options/CommonSystemOption';
 import AppMenu from '@constants/AppMenu';
-import { DialogConfirmation } from '@layout/components/dialogs';
-import { ISelectFieldOption, SelectField } from '@layout/components/fields/SelectField';
+import { FormMode } from '@generic/types';
+import FormikJsonValues from '@layout/components/formik/FormikJsonValues';
 import { FormPage } from '@layout/components/pages/formPage/FormPage';
-import { LookupCompanyOption } from '@lookup/components/company/options/LookupCompanyOption';
+import { SubmissionForm } from '@layout/components/submission/SubmissionForm';
+import { layoutMessage } from '@layout/locales/messages';
 import { lookupMessage } from '@lookup/locales/messages/lookupMessage';
-import { Button, Card, CardContent, CardHeader, TextField } from '@material-ui/core';
-import { Field, FieldProps, Form, Formik, FormikProps } from 'formik';
+import { Form, Formik, FormikProps } from 'formik';
 import * as React from 'react';
 import { ISystemLimitFormValue, SystemLimitFormProps } from './LookupSystemLimitForm';
+import LookupSystemLimitDetailPartialForm from './partial/LookupSystemLimitDetailPartialForm';
 
 export const SystemLimitFormView: React.SFC<SystemLimitFormProps> = props => (
   <FormPage
@@ -16,128 +16,61 @@ export const SystemLimitFormView: React.SFC<SystemLimitFormProps> = props => (
       uid: AppMenu.LookupSystemLimit,
       parentUid: AppMenu.Lookup,
       parentUrl: '/lookup/systemlimits',
-      title: props.intl.formatMessage(lookupMessage.systemLimit.page.newTitle),
-      description: props.intl.formatMessage(lookupMessage.systemLimit.page.newSubHeader)
+      title: props.intl.formatMessage(props.formMode === FormMode.New ? lookupMessage.systemLimit.page.newTitle : lookupMessage.systemLimit.page.modifyTitle),
+      description: props.intl.formatMessage(props.formMode === FormMode.New ? lookupMessage.systemLimit.page.newSubHeader : lookupMessage.systemLimit.page.modifySubHeader)
     }}
     state={props.systemLimitState.detail}
     onLoadApi={props.handleOnLoadApi}
   >
     <Formik
+      enableReinitialize
       initialValues={props.initialValues}
       validationSchema={props.validationSchema}
       onSubmit={props.handleOnSubmit}
       render={(formikBag: FormikProps<ISystemLimitFormValue>) => (
-        <Form style={{display: 'flex', flexDirection: 'column'}}>
-          <Card square style={{maxWidth: 300}}>
-            <CardHeader 
-              title={props.intl.formatMessage(lookupMessage.systemLimit.section.infoTitle)}
-              // subheader={props.intl.formatMessage(lookupMessage.registration.section.infoSubHeader)}
-            />
-            <CardContent>
-              <Field
-                name="companyUid"
-                render={({ field, form }: FieldProps<ISystemLimitFormValue>) => (
-                  <LookupCompanyOption filter={props.filterLookupCompany}>
-                    <SelectField
-                      isSearchable
-                      menuPlacement="auto"
-                      menuPosition="fixed"
-                      isClearable={field.value !== ''}
-                      escapeClearsValue={true}
-                      valueString={field.value}
-                      textFieldProps={{
-                        label: props.intl.formatMessage(lookupMessage.systemLimit.fieldFor(field.name, 'fieldName')),
-                        required: true,
-                        helperText: form.touched.companyUid && form.errors.companyUid,
-                        error: form.touched.companyUid && Boolean(form.errors.companyUid)
-                      }}
-                      onMenuClose={() => formikBag.setFieldTouched(field.name)}
-                      onChange={(selected: ISelectFieldOption) => {
-                        // formikBag.setFieldValue('projectType', 'SPT03');
-                        formikBag.setFieldValue(field.name, selected && selected.value || '');
-                      }}
-                    />
-                  </LookupCompanyOption>
-                )}
-              />
+        <Form>
+          <div className={props.classes.flexRow}>
+            <div className={props.classes.flexColumn}>
+              <div className={props.classes.flexContent}>
+                <LookupSystemLimitDetailPartialForm 
+                  formikBag={formikBag}
+                  formMode={props.formMode}
+                  intl={props.intl}
+                  filterLookupCompany={props.filterLookupCompany}
+                  filterCommonSystem={props.filterCommonSystem}
+                />
+              </div>
+            </div>
 
-              <Field
-                name="categoryType"
-                render={({ field, form }: FieldProps<ISystemLimitFormValue>) => (
-                  <CommonSystemOption category="limiter" filter={props.filterCommonSystem}>
-                    <SelectField
-                      isSearchable
-                      menuPlacement="auto"
-                      menuPosition="fixed"
-                      isClearable={field.value !== ''}
-                      escapeClearsValue={true}
-                      valueString={field.value}
-                      textFieldProps={{
-                        label: props.intl.formatMessage(lookupMessage.systemLimit.fieldFor(field.name, 'fieldName')),
-                        required: true,
-                        helperText: form.touched.categoryType && form.errors.categoryType,
-                        error: form.touched.categoryType && Boolean(form.errors.categoryType)
-                      }}
-                      onMenuClose={() => formikBag.setFieldTouched(field.name)}
-                      onChange={(selected: ISelectFieldOption) => formikBag.setFieldValue(field.name, selected && selected.value || '')}
-                    />
-                  </CommonSystemOption>
-                )}
-              />
+            <div className={props.classes.flexColumn}>
+              <div className={props.classes.flexContent}>
+                <SubmissionForm 
+                  title={props.intl.formatMessage(lookupMessage.shared.submission.form, {state: 'System Limit'})}
+                  className={props.classes.flexContent}
+                  formikProps={formikBag}
+                  buttonLabelProps={{
+                    reset: props.intl.formatMessage(layoutMessage.action.reset),
+                    submit: props.intl.formatMessage(layoutMessage.action.submit),
+                    processing: props.intl.formatMessage(layoutMessage.text.processing)
+                  }}
+                  confirmationDialogProps={{
+                    title: props.intl.formatMessage(props.formMode === FormMode.New ? lookupMessage.shared.confirm.createTitle : lookupMessage.shared.confirm.modifyTitle),
+                    message: props.intl.formatMessage(props.formMode === FormMode.New ? lookupMessage.shared.confirm.createDescription : lookupMessage.shared.confirm.modifyDescription, {state: 'System Limit'}),
+                    labelCancel: props.intl.formatMessage(layoutMessage.action.discard),
+                    labelConfirm: props.intl.formatMessage(layoutMessage.action.continue)
+                  }} 
+                />
+              </div>
+            </div>
 
-              <Field
-                name="days"
-                render={({ field, form }: FieldProps<ISystemLimitFormValue>) => (
-                  <TextField 
-                    {...field}
-                    fullWidth
-                    required={true}
-                    margin="normal"
-                    autoComplete="off"
-                    disabled={form.isSubmitting}
-                    label={props.intl.formatMessage(lookupMessage.systemLimit.fieldFor(field.name, 'fieldName'))}
-                    placeholder={props.intl.formatMessage(lookupMessage.systemLimit.fieldFor(field.name, 'fieldPlaceholder'))}
-                    helperText={form.touched.days && form.errors.days}
-                    error={form.touched.days && Boolean(form.errors.days)}
-                  />
-                )}
-              />
-
-            </CardContent>
-          </Card>
-
-          <pre>{JSON.stringify(formikBag.values, null, 2)}</pre>
-
-          <Button
-            type="reset"
-            variant="contained"
-            color="secondary"
-            disabled={!formikBag.dirty}
-          >
-            Reset
-          </Button>
-
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={formikBag.isSubmitting}
-          >
-            {formikBag.isSubmitting ? 'Processing' : 'Submit'}
-          </Button>
+            <div className={props.classes.flexColumn}>
+              <div className={props.classes.flexContent}>
+                <FormikJsonValues formikBag={formikBag} />
+              </div>
+            </div>
+          </div>
         </Form>
       )}
-    />
-
-    <DialogConfirmation 
-      isOpen={props.dialogOpen}
-      fullScreen={props.dialogFullScreen}
-      title={props.dialogTitle}
-      content={props.dialogContent}
-      labelCancel={props.dialogCancelLabel}
-      labelConfirm={props.dialogConfirmLabel}
-      onClickCancel={props.handleOnCloseDialog}
-      onClickConfirm={props.handleOnConfirm}
     />
   </FormPage>
 );
