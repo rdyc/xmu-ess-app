70046a175bcf2fdaa068500e442901c1d78b0564
hammadaf9408 <hammadaf9408@gmail.com>
2019-04-05 17:25:56 +0700

    (Mileage) Upgrade detail MG Item

--- src/modules/mileage/components/request/detail/shared/TimeSheetItem.tsx
+++ src/modules/mileage/components/request/detail/shared/TimeSheetItem.tsx
@@ -2,7 +2,6 @@ import { GlobalFormat } from '@layout/types';
 import { GlobalStyle } from '@layout/types/GlobalStyle';
 import {
   Card,
-  CardContent,
   CardHeader,
   Collapse,
   Divider,
@@ -11,10 +10,13 @@ import {
   ListItemSecondaryAction,
   ListItemText,
   TextField,
+  WithStyles,
+  withStyles,
 } from '@material-ui/core';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import { mileageMessage } from '@mileage/locales/messages/mileageMessage';
+import styles from '@styles';
 import { ITimesheet } from '@timesheet/classes/response';
 import { timesheetMessage } from '@timesheet/locales/messages/timesheetMessage';
 import * as React from 'react';
@@ -38,6 +40,7 @@ type AllProps
   = OwnProps
   & OwnState
   & OwnStateHandler
+  & WithStyles<typeof styles>
   & InjectedIntlProps;
 
 const createProps: mapper<AllProps, OwnState> = (): OwnState => ({
@@ -62,137 +65,136 @@ const timesheetItem: React.SFC<AllProps> = props => {
         title={props.intl.formatMessage(mileageMessage.request.timesheet.title)}
         // subheader={props.intl.formatMessage(mileageMessage.request.timesheet.subHeader)}
       />
-      <CardContent>
-        <List>
-          {
-            data &&
-            data.map((item, index) =>
-              <React.Fragment key={item.uid}>
-                <ListItem 
-                  disableGutters
-                  button
-                  selected={item.uid === active && isExpanded}
-                  onClick={() => handleToggle(item.uid)}
-                >
-                  <ListItemText primary={item.uid} secondary={`${intl.formatDate(item.date, GlobalFormat.Date)} | ${props.intl.formatTime(item.start, GlobalFormat.Time)} - ${props.intl.formatTime(item.end, GlobalFormat.Time)}`}/>
-                  <ListItemSecondaryAction>
-                    {active === item.uid && isExpanded ? <ExpandLess /> : <ExpandMore />}
-                  </ListItemSecondaryAction>
-                </ListItem>
-                {len !== index && <Divider />}
-                <Collapse
-                  in={active === item.uid && isExpanded}
-                  timeout="auto"
-                  unmountOnExit
-                >
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.statusType)}
-                    value={item.status ? item.status.value : item.statusType}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"                    
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.uid)}
-                    value={item.uid}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.date)}
-                    value={props.intl.formatDate(item.date, GlobalFormat.Date)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.activityType)}
-                    value={item.activity ? item.activity.value : 'N/A'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
+      <List>
+        {
+          data &&
+          data.map((item, index) =>
+            <React.Fragment key={item.uid}>
+              <Divider />
+              <ListItem 
+                button
+                selected={item.uid === active && isExpanded}
+                onClick={() => handleToggle(item.uid)}
+              >
+                <ListItemText primary={item.uid} secondary={`${intl.formatDate(item.date, GlobalFormat.Date)} | ${props.intl.formatTime(item.start, GlobalFormat.Time)} - ${props.intl.formatTime(item.end, GlobalFormat.Time)}`}/>
+                <ListItemSecondaryAction>
+                  {active === item.uid && isExpanded ? <ExpandLess /> : <ExpandMore />}
+                </ListItemSecondaryAction>
+              </ListItem>
+              {len !== index && <Divider />}
+              <Collapse
+                in={active === item.uid && isExpanded}
+                className={props.classes.paddingFar}
+                timeout="auto"
+                unmountOnExit
+              >
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.statusType)}
+                  value={item.status ? item.status.value : item.statusType}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"                    
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.uid)}
+                  value={item.uid}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.date)}
+                  value={props.intl.formatDate(item.date, GlobalFormat.Date)}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.activityType)}
+                  value={item.activity ? item.activity.value : 'N/A'}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
 
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.customerUid)}
-                    value={item.customer ? item.customer.name : 'N/A'}
-                  />
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.customerUid)}
+                  value={item.customer ? item.customer.name : 'N/A'}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                  multiline={true}
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.projectUid)}
+                  value={item.project ? `${item.project.uid} - ${item.project.name}` : 'N/A'}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.siteUid)}
+                  value={item.site ? item.site.name : 'N/A'}
+                />
+                {(item.mileageExceptionUid) ?
                   <TextField
                     {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
+                  margin="dense"
+
+                    label={props.intl.formatMessage(timesheetMessage.entry.field.siteValue)}
+                    value={props.intl.formatNumber(((item.mileageException ? item.mileageException.percentage : 0) / 100) * (item.site ? item.site.value : 0))}
+                  /> :
+                  <TextField
+                    {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                    label={props.intl.formatMessage(timesheetMessage.entry.field.siteValue)}
+                    value={props.intl.formatNumber(item.site ? item.site.value : 0)}
+                  />
+                }
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.start)}
+                  value={props.intl.formatTime(item.start, GlobalFormat.Time)}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.end)}
+                  value={props.intl.formatTime(item.end, GlobalFormat.Time)}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.totalHours)}
+                  value={item.hours ? item.hours : 0}
+                />
+                <TextField
+                  {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
+
+                  multiline={true}
+                  label={props.intl.formatMessage(timesheetMessage.entry.field.notes)}
+                  value={item.description || 'N/A'}
+                />
+                {(item.mileageExceptionUid) ?
+                  <TextField
+                    {...GlobalStyle.TextField.ReadOnly}
+                  margin="dense"
 
                     multiline={true}
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.projectUid)}
-                    value={item.project ? `${item.project.uid} - ${item.project.name}` : 'N/A'}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.siteUid)}
-                    value={item.site ? item.site.name : 'N/A'}
-                  />
-                  {(item.mileageExceptionUid) ?
-                    <TextField
-                      {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                      label={props.intl.formatMessage(timesheetMessage.entry.field.siteValue)}
-                      value={props.intl.formatNumber(((item.mileageException ? item.mileageException.percentage : 0) / 100) * (item.site ? item.site.value : 0))}
-                    /> :
-                    <TextField
-                      {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                      label={props.intl.formatMessage(timesheetMessage.entry.field.siteValue)}
-                      value={props.intl.formatNumber(item.site ? item.site.value : 0)}
-                    />
-                  }
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.start)}
-                    value={props.intl.formatTime(item.start, GlobalFormat.Time)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.end)}
-                    value={props.intl.formatTime(item.end, GlobalFormat.Time)}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.totalHours)}
-                    value={item.hours ? item.hours : 0}
-                  />
-                  <TextField
-                    {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                    multiline={true}
-                    label={props.intl.formatMessage(timesheetMessage.entry.field.notes)}
-                    value={item.description || 'N/A'}
-                  />
-                  {(item.mileageExceptionUid) ?
-                    <TextField
-                      {...GlobalStyle.TextField.ReadOnly}
-                    margin="dense"
-
-                      multiline={true}
-                      label={props.intl.formatMessage(timesheetMessage.entry.field.mileageException)}
-                      value={item.mileageException && item.mileageException.reason || 'N/A'}
-                    /> : ''
-                  }
-                  {isExpanded && <Divider />}
-                </Collapse>
-              </React.Fragment>
-            )
-          }
-        </List>
-      </CardContent>
+                    label={props.intl.formatMessage(timesheetMessage.entry.field.mileageException)}
+                    value={item.mileageException && item.mileageException.reason || 'N/A'}
+                  /> : ''
+                }
+                {isExpanded && <Divider />}
+              </Collapse>
+            </React.Fragment>
+          )
+        }
+      </List>
     </Card>
   );
 
@@ -201,5 +203,6 @@ const timesheetItem: React.SFC<AllProps> = props => {
 
 export const TimesheetItem = compose<AllProps, OwnProps>(
   injectIntl,
+  withStyles(styles),
   withStateHandlers<OwnState, OwnStateHandler>(createProps, stateUpdaters)
 )(timesheetItem);
