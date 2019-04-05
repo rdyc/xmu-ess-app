044fef68397c20264eb443a2dabe94b3aff34051
cptodix <cptodix@gmail.com>
2019-04-05 19:22:48 +0700

    ketinggalan lagi

--- src/modules/expense/components/request/form/partials/ExpenseDetailPartialForm.tsx
+++ src/modules/expense/components/request/form/partials/ExpenseDetailPartialForm.tsx
@@ -171,11 +171,11 @@ const ExpenseDetailPartialForm: React.ComponentType<ExpenseDetailPartialFormProp
             }}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               // set current field
-              props.formikBag.setFieldValue(field.name, parseFloat(e.target.value));
-
               if (e.target.value === '') {
                 props.formikBag.setFieldValue(field.name, parseFloat('0'));
-              } 
+              } else {
+                props.formikBag.setFieldValue(field.name, parseFloat(e.target.value));
+              }
             }}
           />
         )}
