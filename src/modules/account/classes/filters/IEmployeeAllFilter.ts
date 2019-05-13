8fac987c743888777c9b7eb91b1c0226dccba8f4
hammadaf9408 <hammadaf9408@gmail.com>
2019-05-13 13:24:37 +0700

    (Employee) Filter company

--- src/modules/account/classes/filters/IEmployeeAllFilter.ts
+++ src/modules/account/classes/filters/IEmployeeAllFilter.ts
@@ -2,6 +2,7 @@ import { IBasePagingFilter } from '@generic/interfaces';
 
 export interface IEmployeeAllFilter extends IBasePagingFilter {
   companyUids?: string;
+  useAccess?: boolean | false;
   roleUids?: string;
   positionUids?: string;
 }
