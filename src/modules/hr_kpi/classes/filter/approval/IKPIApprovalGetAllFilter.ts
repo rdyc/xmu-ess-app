7dc6bdeb6f3ec5d9d6e346730d1bc22829b10b32
cptodix <cptodix@gmail.com>
2019-09-17 15:21:29 +0700

    add filter param

--- src/modules/hr_kpi/classes/filter/approval/IKPIApprovalGetAllFilter.ts
+++ src/modules/hr_kpi/classes/filter/approval/IKPIApprovalGetAllFilter.ts
@@ -4,4 +4,5 @@ export interface IKPIApprovalGetAllFilter extends IBasePagingFilter {
   companyUid?: string;
   statusTypes?: string;
   status?: 'complete' | 'pending' | string;
+  isFinal?: boolean;
 }
--- src/modules/hr_kpi/classes/filter/employee/IKPIEmployeeGetAllFilter.ts
+++ src/modules/hr_kpi/classes/filter/employee/IKPIEmployeeGetAllFilter.ts
@@ -3,4 +3,5 @@ import { IBasePagingFilter } from '@generic/interfaces';
 export interface IKPIEmployeeGetAllFilter extends IBasePagingFilter {
   statusTypes?: string;
   status?: 'complete' | 'pending' | string;
+  isFinal?: boolean;
 }
