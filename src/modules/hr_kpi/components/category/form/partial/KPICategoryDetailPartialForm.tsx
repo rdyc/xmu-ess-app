a4aa8b9fe72c3919ff3390bf697bf054adb49c0c
cptodix <cptodix@gmail.com>
2019-09-23 14:36:38 +0700

    minifixes

--- src/modules/hr_kpi/components/category/form/partial/KPICategoryDetailPartialForm.tsx
+++ src/modules/hr_kpi/components/category/form/partial/KPICategoryDetailPartialForm.tsx
@@ -4,6 +4,7 @@ import { kpiMessage } from '@kpi/locales/messages/kpiMessage';
 import { ISelectFieldOption, SelectField } from '@layout/components/fields/SelectField';
 import { InputCategoryGroupOption } from '@layout/components/input/categoryGroup';
 import { layoutMessage } from '@layout/locales/messages';
+import { GlobalStyle } from '@layout/types/GlobalStyle';
 import { Card, CardContent, CardHeader, TextField } from '@material-ui/core';
 import { Field, FieldProps, FormikProps } from 'formik';
 import * as React from 'react';
@@ -27,9 +28,9 @@ const KPICategoryDetailPartialForm: React.ComponentType<KPIcategoryDetailPartial
         name="uid"
         render={({ field }: FieldProps<IKPICategoryFormValue>) => (
           <TextField
+            {...GlobalStyle.TextField.ReadOnly}
             {...field}
-            fullWidth
-            disabled
+            disabled={props.formikBag.isSubmitting}
             margin="normal"
             label={props.intl.formatMessage(kpiMessage.category.field.uid)}
             helperText={props.formMode === FormMode.New && props.intl.formatMessage(layoutMessage.text.autoField)}
