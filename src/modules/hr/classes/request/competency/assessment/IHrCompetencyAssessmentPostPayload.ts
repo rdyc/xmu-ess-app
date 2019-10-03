833cf7b7bf13a8a9a6da45a344da3077eabe7fef
hammadaf9408 <hammadaf9408@gmail.com>
2019-10-03 14:37:07 +0700

    (Hr Assessment) Adjust with API

--- src/modules/hr/classes/request/competency/assessment/IHrCompetencyAssessmentPostPayload.ts
+++ src/modules/hr/classes/request/competency/assessment/IHrCompetencyAssessmentPostPayload.ts
@@ -2,6 +2,7 @@ import { IAssessmentItemCreate } from './IAssessmentItemCreate';
 
 export interface IHrCompetencyAssessmentPostPayload {
   employeeUid: string;
+  companyUid: string;
   positionUid: string;
   assessmentYear: number;
   responders: IAssessmentItemCreate[];
--- src/modules/hr/classes/request/competency/assessment/IHrCompetencyAssessmentPutPayload.ts
+++ src/modules/hr/classes/request/competency/assessment/IHrCompetencyAssessmentPutPayload.ts
@@ -2,6 +2,7 @@ import { IAssessmentItemCreate } from './IAssessmentItemCreate';
 
 export interface IHrCompetencyAssessmentPutPayload {
   employeeUid: string;
+  companyUid: string;
   positionUid: string;
   assessmentYear: number;
   responders: IAssessmentItemCreate[];
