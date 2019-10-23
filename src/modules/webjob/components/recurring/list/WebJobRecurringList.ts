51f75576e47357091a84dc9233af3c58867514d4
hammadaf9408 <hammadaf9408@gmail.com>
2019-10-23 19:01:22 +0700

    (WebJob Recurring) List and detail

---
+++ src/modules/webjob/components/recurring/list/WebJobRecurringList.ts
@@ -0,0 +1,146 @@
+import { IBasePagingFilter } from '@generic/interfaces';
+import { ICollectionValue } from '@layout/classes/core';
+import { IDataBindResult } from '@layout/components/pages';
+import { WithUser, withUser } from '@layout/hoc/withUser';
+import { WithStyles, withStyles } from '@material-ui/core';
+import styles from '@styles';
+import { IWebJobRecurringGetAllFilter } from '@webjob/classes/filters';
+import { IWebJobRecurring } from '@webjob/classes/response';
+import { IWebJobRequestField } from '@webjob/classes/types';
+import { withWebJobRecurring, WithWebJobRecurring } from '@webjob/hoc/withWebJobRecurring';
+// import * as moment from 'moment';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { RouteComponentProps, withRouter } from 'react-router';
+import {
+  compose,
+  HandleCreators,
+  mapper,
+  setDisplayName,
+  shallowEqual,
+  StateHandlerMap,
+  StateUpdaters,
+  withHandlers,
+  withStateHandlers,
+} from 'recompose';
+
+import { WebJobRecurringListView } from './WebJobRecurringListView';
+
+interface IOwnOption {
+  
+}
+
+interface IOwnParams {
+  // type: string;
+}
+
+interface IOwnState {
+  fields: ICollectionValue[];
+}
+
+interface IOwnStateUpdater extends StateHandlerMap<IOwnState> {
+
+}
+
+interface IOwnHandler {
+  handleOnLoadApi: (filter?: IBasePagingFilter, resetPage?: boolean, isRetry?: boolean) => void;
+  handleOnLoadApiSearch: (find?: string, findBy?: string) => void;
+  handleOnBind: (item: IWebJobRecurring, index: number) => IDataBindResult;
+}
+
+export type WebJobRecurringListProps
+  = IOwnOption
+  & IOwnState
+  & IOwnStateUpdater
+  & IOwnHandler
+  & InjectedIntlProps
+  & RouteComponentProps<IOwnParams>
+  & WithStyles<typeof styles>
+  & WithUser
+  & WithWebJobRecurring;
+
+const createProps: mapper<IOwnOption, IOwnState> = (): IOwnState => {
+  const state: IOwnState = {
+    fields: Object.keys(IWebJobRequestField).map(key => ({
+      value: key,
+      name: IWebJobRequestField[key]
+    })),
+  };
+
+  return state;
+};
+
+const stateUpdaters: StateUpdaters<WebJobRecurringListProps, IOwnState, IOwnStateUpdater> = {
+};
+
+const handlerCreators: HandleCreators<WebJobRecurringListProps, IOwnHandler> = {
+  handleOnLoadApi: (props: WebJobRecurringListProps) => (params?: IBasePagingFilter, resetPage?: boolean, isRetry?: boolean) => {
+    const { loadAllRequest } = props.webJobRecurringDispatch;
+    const { isExpired, isLoading, request } = props.webJobRecurringState.all;
+
+    if (props.userState.user && !isLoading) {
+      const filter: IWebJobRecurringGetAllFilter = {
+        find: request && request.filter && request.filter.find,
+        findBy: request && request.filter && request.filter.findBy,
+        orderBy: params && params.orderBy || request && request.filter && request.filter.orderBy,
+        direction: params && params.direction || request && request.filter && request.filter.direction,
+        page: resetPage ? undefined : params && params.page || request && request.filter && request.filter.page,
+        size: params && params.size || request && request.filter && request.filter.size
+      };
+
+      // when request is defined, then compare the filter props
+      const shouldLoad = !shallowEqual(filter, request && request.filter || {});
+      
+      // only load when request parameter are differents
+      if (isExpired || shouldLoad || isRetry) {
+        loadAllRequest({
+          filter
+        });
+      }
+    }
+  },
+  handleOnLoadApiSearch: (props: WebJobRecurringListProps) => (find?: string, findBy?: string) => {
+    const { isLoading, request } = props.webJobRecurringState.all;
+    const { loadAllRequest } = props.webJobRecurringDispatch;
+    const { user } = props.userState;
+
+    if (user && !isLoading) {
+      // predefined filter
+      const filter = {
+        ...request && request.filter,
+        find,
+        findBy,
+        page: undefined
+      };
+      
+      // compare request
+      const shouldLoad = !shallowEqual(filter, request && request.filter || {});
+      
+      // only load when request parameter are differents
+      if (shouldLoad) {
+        loadAllRequest({
+          filter
+        });
+      }
+    }
+  },
+  handleOnBind: (props: WebJobRecurringListProps) => (item: IWebJobRecurring, index: number) => ({
+    key: index,
+    primary: item.uid,
+    secondary: item.name,
+    tertiary: item.description,
+    quaternary: '',
+    quinary: item.cron.expression,
+    senary: item.isAutoStart.toString()
+  }),
+};
+
+export const WebJobRecurringList = compose<WebJobRecurringListProps, IOwnOption>(
+  setDisplayName('WebJobRecurringList'),
+  withUser,
+  withRouter,
+  withWebJobRecurring,
+  injectIntl,
+  withStateHandlers(createProps, stateUpdaters),
+  withHandlers(handlerCreators),
+  withStyles(styles)
+)(WebJobRecurringListView);
