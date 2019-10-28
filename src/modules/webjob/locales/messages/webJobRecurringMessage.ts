b1b73cc6fc90f474abcf2168ddfb2b6fc3a69b7c
hammadaf9408 <hammadaf9408@gmail.com>
2019-10-28 15:12:32 +0700

    Messages

--- src/modules/webjob/locales/messages/webJobRecurringMessage.ts
+++ src/modules/webjob/locales/messages/webJobRecurringMessage.ts
@@ -14,7 +14,9 @@ export const webJobRecurringField = defineMessages({
   namespace: {id: `${prefix}.field.namespace`},
   class: {id: `${prefix}.field.class`},
   method: {id: `${prefix}.field.method`},
-  isAutoStart: {id: `${prefix}.field.isAutoStart`},
+  startType: {id: `${prefix}.field.start.type`},
+  isAutoStart: {id: `${prefix}.field.start.auto`},
+  isManualStart: {id: `${prefix}.field.start.manual`},
   definition: {id: `${prefix}.field.definition`},
 
   // FREE TYPE
