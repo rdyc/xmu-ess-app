e0a86a5451a58043c7c49af8a9eef2d995fb3c75
hammadaf9408 <hammadaf9408@gmail.com>
2019-10-28 11:53:44 +0700

    (HR Assessment) Detail, give some help for color desc

--- src/modules/hr/components/competency/detail/assessment/HrAssessmentResponderItem.tsx
+++ src/modules/hr/components/competency/detail/assessment/HrAssessmentResponderItem.tsx
@@ -6,75 +6,145 @@ import {
   Card,
   CardContent,
   CardHeader,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Typography,
   WithStyles,
   withStyles,
 } from '@material-ui/core';
+import { HelpOutline } from '@material-ui/icons';
 import styles from '@styles';
 import * as classNames from 'classnames';
 import * as React from 'react';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
-import { compose } from 'recompose';
+import { compose, mapper, StateHandlerMap, StateUpdaters, withStateHandlers } from 'recompose';
 
-interface OwnProps {
+interface IOwnProps {
   data: IHrCompetencyAssessmentDetail;
 }
 
-type AllProps = OwnProps & InjectedIntlProps & WithStyles<typeof styles>;
+interface IOwnState {
+  isHelpOpen: boolean;
+}
+
+interface IOwnStateHandler extends StateHandlerMap<IOwnState> {
+  handleToggle: () => IOwnState;
+}
+
+type AllProps 
+  = IOwnProps 
+  & IOwnState
+  & IOwnStateHandler
+  & InjectedIntlProps 
+  & WithStyles<typeof styles>;
+
+const createProps: mapper<AllProps, IOwnState> = (): IOwnState => ({
+  isHelpOpen: false,
+});
+
+const stateUpdaters: StateUpdaters<{}, IOwnState, IOwnStateHandler> = {
+  handleToggle: (state: IOwnState) => () => ({
+    isHelpOpen: !state.isHelpOpen
+  })
+};
 
 const hrAssessmentResponderItem: React.SFC<AllProps> = props => {
-  const { data, intl } = props;
+  const { data, intl, handleToggle } = props;
 
   const render = (
-    <Card square>
-      <CardHeader
-        title={intl.formatMessage(hrMessage.shared.section.infoTitle, {state: 'Responder'})}
-      />
-      <CardContent>
-        <List>
-        {
-          data.responders &&
-          data.responders.length === 0 &&
-          <ListItem>
-            <ListItemText
-              primary={intl.formatMessage(hrMessage.competency.field.notFound, {state: 'Responder'})}
-              primaryTypographyProps={{
-                align: 'center'
-              }} 
-            />
-          </ListItem>
-        }
-        {
-          data.responders &&
-          data.responders.map(item =>
-            <ListItem 
-              disableGutters 
-              key={item.employeeUid}
+    <React.Fragment>      
+      <Card square>
+        <CardHeader
+          title={intl.formatMessage(hrMessage.shared.section.infoTitle, {state: 'Responder'})}
+          action={
+            <IconButton
+              onClick={() => handleToggle()}
             >
-              <ListItemAvatar>
-                <Avatar className={classNames(!item.isExpired && !item.isRespond ? props.classes.avatarSecondary : (item.isExpired ? props.classes.avatarRed : (item.isRespond && props.classes.avatarPrimary || '')))}>
-                  {initialName(item.employee.fullName)}
-                </Avatar>
-              </ListItemAvatar>
+              <HelpOutline />
+            </IconButton>
+          }
+        />
+        <CardContent>
+          <List>
+          {
+            data.responders &&
+            data.responders.length === 0 &&
+            <ListItem>
               <ListItemText
-                primary={item.employee.fullName} 
-                secondary={item.employee.email}
+                primary={intl.formatMessage(hrMessage.competency.field.notFound, {state: 'Responder'})}
+                primaryTypographyProps={{
+                  align: 'center'
+                }} 
               />
             </ListItem>
-          )
-        }
-        </List>
-      </CardContent>
-    </Card>
+          }
+          {
+            data.responders &&
+            data.responders.map(item =>
+              <ListItem 
+                disableGutters 
+                key={item.employeeUid}
+              >
+                <ListItemAvatar>
+                  <Avatar className={classNames(!item.isExpired && !item.isRespond ? props.classes.avatarSecondary : (item.isExpired ? props.classes.avatarRed : (item.isRespond && props.classes.avatarPrimary || '')))}>
+                    {initialName(item.employee.fullName)}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={item.employee.fullName} 
+                  secondary={item.employee.email}
+                />
+              </ListItem>
+            )
+          }
+          </List>
+        </CardContent>
+      </Card>
+      <Dialog maxWidth="xs" onClose={() => handleToggle()} open={props.isHelpOpen} >
+        <DialogTitle>
+          Color description
+        </DialogTitle>
+        <DialogContent>
+          <Grid container spacing={24}>
+            <Grid item xs={12} style={{display: 'flex'}}>
+            <Avatar className={props.classes.avatarSecondary}>
+            </Avatar>
+              <Typography style={{position: 'absolute', left: '72px', transform: 'translate(10px, 10px)'}}>
+                No Respond
+              </Typography>
+            </Grid>
+            <Grid item xs={12} style={{display: 'flex'}}>
+            <Avatar className={props.classes.avatarPrimary}>
+            </Avatar>
+              <Typography style={{position: 'absolute', left: '72px', transform: 'translate(10px, 10px)'}}>
+                Respond
+              </Typography>
+            </Grid>
+            <Grid item xs={12} style={{display: 'flex'}}>
+            <Avatar className={props.classes.avatarRed}>
+            </Avatar>
+              <Typography style={{position: 'absolute', left: '72px', transform: 'translate(10px, 10px)'}}>
+                Expired
+              </Typography>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
   );
 
   return render;
 };
 
-export const HrAssessmentResponderItem = compose<AllProps,  OwnProps>(
+export const HrAssessmentResponderItem = compose<AllProps, IOwnProps>(
   injectIntl,
-  withStyles(styles)
+  withStyles(styles),
+  withStateHandlers(createProps, stateUpdaters)
 )(hrAssessmentResponderItem);
