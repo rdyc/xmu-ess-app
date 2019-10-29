f250e988e9851ada041126f4841eef74a59e3406
hammadaf9408 <hammadaf9408@gmail.com>
2019-10-29 17:32:30 +0700

    (KPI Final) Buang created by

--- src/modules/myprofile/components/detail/kpi/detail/shared/MyKPIFinalSummary.tsx
+++ src/modules/myprofile/components/detail/kpi/detail/shared/MyKPIFinalSummary.tsx
@@ -51,13 +51,6 @@ const myKPIFinalSummary: React.SFC<AllProps> = props => (
     {
       props.data.changes &&
       <Grid item xs={12} sm={6} md={3}>
-        <TextField
-          {...GlobalStyle.TextField.ReadOnly}
-          label={props.intl.formatMessage(layoutMessage.field.createdBy)}
-          value={props.data.changes.created && props.data.changes.created.fullName || 'N/A'}
-          helperText={props.intl.formatDate(props.data.changes.createdAt, GlobalFormat.DateTime) || 'N/A'}
-        />
-
         {
           (props.data.changes.updated && props.data.changes.updatedAt) &&
           <TextField
